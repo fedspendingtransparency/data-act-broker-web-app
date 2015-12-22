ec2ad881f6b8b55d4d8b882c9d31263d960564b8
Katie Rose <rose_katherine@bah.com>
2015-12-22 10:30:45 -0500

    Moving registration confirmation code to jsx based on updated routing.

--- src/js/components/RegistrationComponents/ConfirmCodeComponent.jsx
+++ src/js/components/RegistrationComponents/ConfirmCodeComponent.jsx
@@ -3,13 +3,29 @@
 * Created by Kyle Fox 12/11/15
 **/
 
-import React from 'react';
+import React, { PropTypes } from 'react';
+import Navbar from '../SharedComponents/NavigationComponents.jsx';
+import TextInputComponent from '../SharedComponents/TextInputComponent.jsx';
+import PasswordInputComponent from '../SharedComponents/PasswordInputComponent.jsx';
+import DiscreteProgressBarComponent from '../SharedComponents/DiscreteProgressBarComponent.jsx';
 
 // An email input field that does basic validation for .mil and .gov emails
 export default class ConfirmCode extends React.Component {
     render() {
         return (
-            <h1>Confirm</h1>
+            <div>
+                <Navbar />
+                <div className="usa-da-content">
+                    <DiscreteProgressBarComponent progressCurrentStep="3" progressTotalSteps="4" />
+                    <h1>Your email has been verified!</h1>
+                    <p>Please continue the registration process by providing the following information.</p>
+                    <p><TextInputComponent inputClass="" inputPlaceholder="First Name" inputName="regFirstName" /></p>
+                    <p><TextInputComponent inputClass="" inputPlaceholder="Last Name" inputName="regLastName" /></p>
+                    <p><TextInputComponent inputClass="" inputPlaceholder="Phone Number" inputName="regPhoneNumber" /></p>
+                    <p><PasswordInputComponent inputClass="" inputPlaceholder="Password" inputName="regPassword" /></p>
+                    <p><PasswordInputComponent inputClass="" inputPlaceholder="Confirm Password" inputName="regConfirmPassword" /></p>
+                </div>
+            </div>
         );
     }
 }
