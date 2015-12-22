e22ad557e894506a7b9711e4dc78e912e0ad8cc5
Katie Rose <rose_katherine@bah.com>
2015-12-22 10:23:25 -0500

    Revert "Moving registration confirmation code into correct jsx based on routing."
    This reverts commit 6335b45612095a9fc7ea0cf4acd5c3f1a1c8e29f.

--- src/js/components/RegistrationComponents/ConfirmCodeComponent.jsx
+++ src/js/components/RegistrationComponents/ConfirmCodeComponent.jsx
@@ -3,29 +3,13 @@
 * Created by Kyle Fox 12/11/15
 **/
 
-import React, { PropTypes } from 'react';
-import Navbar from '../SharedComponents/NavigationComponents.jsx';
-import TextInputComponent from '../SharedComponents/TextInputComponent.jsx';
-import PasswordInputComponent from '../SharedComponents/PasswordInputComponent.jsx';
-import DiscreteProgressBarComponent from '../SharedComponents/DiscreteProgressBarComponent.jsx';
+import React from 'react';
 
 // An email input field that does basic validation for .mil and .gov emails
 export default class ConfirmCode extends React.Component {
     render() {
         return (
-            <div>
-                <Navbar />
-                <div className="usa-da-content">
-                    <DiscreteProgressBarComponent progressCurrentStep="3" progressTotalSteps="4" />
-                    <h1>Your email has been verified!</h1>
-                    <p>Please continue the registration process by providing the following information.</p>
-                    <p><TextInputComponent inputClass="" inputPlaceholder="First Name" inputName="regFirstName" /></p>
-                    <p><TextInputComponent inputClass="" inputPlaceholder="Last Name" inputName="regLastName" /></p>
-                    <p><TextInputComponent inputClass="" inputPlaceholder="Phone Number" inputName="regPhoneNumber" /></p>
-                    <p><PasswordInputComponent inputClass="" inputPlaceholder="Password" inputName="regPassword" /></p>
-                    <p><PasswordInputComponent inputClass="" inputPlaceholder="Confirm Password" inputName="regConfirmPassword" /></p>
-                </div>
-            </div>
+            <h1>Confirm</h1>
         );
     }
 }
