7170141366688451a4c9ae00ed11895b093bbec6
Michael Bray <bray_michael@bah.com>
2015-12-30 15:56:41 -0500

    Updated Landing page with new grid framework, refactored some common CSS

--- src/js/components/LandingComponents.jsx
+++ src/js/components/LandingComponents.jsx
@@ -11,24 +11,31 @@ class LandingContent extends React.Component {
     render() {
         return (
             <div>
-                <div className="usa-da-content usa-da-content-dark">
-                    <div className="usa-grid">
-                        <div className="usa-width-one-half">
-                            <h1>Welcome to the Data Broker</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.
-                                Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar
-                                tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                                Nam fermentum, nulla luctus pharetra vulputate, felis</p>
+                <div className="usa-da-content-dark">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <h1>Welcome to the Data Broker</h1>
+                                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.
+                                    Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar
+                                    tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                                    Nam fermentum, nulla luctus pharetra vulputate, felis</h3>
+                            </div>
+                            <div className="col-md-6">
+
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="usa-da-content">
-                    <div className="usa-grid">
-                        <div className="usa-width-one-half">
-                            <a className="usa-da-button-landing" href="/#/addData">Add & Validate New Data</a>
-                        </div>
-                        <div className="usa-width-one-half">
-                            <a className="usa-da-button-landing" href="#">Performance Dashboard</a>
+                <div>
+                    <div className="container">
+                        <div className="row usa-da-button-landing-holder">
+                            <div className="col-md-6">
+                                <a className="usa-da-button-landing" href="/#/addData">Add & Validate New Data</a>
+                            </div>
+                            <div className="col-md-6">
+                                <a className="usa-da-button-landing" href="#">Performance Dashboard</a>
+                            </div>
                         </div>
                     </div>
                 </div>
