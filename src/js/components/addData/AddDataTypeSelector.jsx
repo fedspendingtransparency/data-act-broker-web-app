5101946f9b187b66c0e8feea03fec150efa39ab9
Katie Rose <rose_katherine@bah.com>
2016-01-04 21:32:23 -0500

    Set styling for addData file type selection

--- src/js/components/addData/AddDataTypeSelector.jsx
+++ src/js/components/addData/AddDataTypeSelector.jsx
@@ -8,14 +8,14 @@ import React from 'react';
 export default class TypeSelector extends React.Component {
  	  render() {
  		    return (
-            <div className="usa-da-color-gray-light-half-tone usa-da-login-container usa-grid">
-                <div className="usa-width-one-whole">
-                    <p>
-                        Three ways to submit your data...
-                        <button className="usa-button-big" type="submit" value="Upload CSV Files">Upload CSV Files</button>
-                        <button className="usa-button-big" type="submit" value="Upload XBRL File" disabled={true}>Upload XBRL File</button>
-                        <button className="usa-button-big" type="submit" value="Setup a Data Feed" disabled={true}>Setup a Data Feed</button>
-                    </p>
+            <div className="usa-da-color-gray-light-half-tone usa-da-login-container usa-grid container">
+                <div className="usa-width-one-whole row center-block">
+                    <h2>Three ways to submit your data...</h2>
+                    <div className="row text-center">
+                        <div className="col-md-4"><button className="usa-da-button-biggest" type="submit" value="Upload CSV Files">Upload CSV Files</button></div>
+                        <div className="col-md-4"><button className="usa-da-button-biggest" type="submit" value="Upload XBRL File" disabled={true}>Upload XBRL File</button></div>
+                        <div className="col-md-4"><button className="usa-da-button-biggest" type="submit" value="Setup a Data Feed" disabled={true}>Setup a Data Feed</button></div>
+                    </div>
                 </div>
             </div>
  		    );
