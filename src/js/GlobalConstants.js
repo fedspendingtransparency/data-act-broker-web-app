bc2d73c49786c483192409aff205dc182e735740
Kyle Fox <kyle.fox55@gmail.com>
2016-02-05 11:22:11 -0500

    Conditionally set API link in anticipation of 2 separate paths for prod and dev

--- src/js/GlobalConstants.js
+++ src/js/GlobalConstants.js
@@ -1,3 +1,5 @@
+const apiConst = (process.env.NODE_ENV === 'production') ? null : 'http://ec2-54-173-199-34.compute-1.amazonaws.com:80/v1/';
+
 export const kGlobalConstants = {
-    API: 'http://ec2-54-173-199-34.compute-1.amazonaws.com:80/v1/'
+    API: apiConst
 };
