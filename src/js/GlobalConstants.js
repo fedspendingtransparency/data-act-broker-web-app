d2b96377a74e4319e2e08cb17ce2de7f502f8856
Kyle Fox <kyle.fox55@gmail.com>
2016-02-11 11:53:46 -0500

    Removing the generated GlobalConstants.js file

--- src/js/GlobalConstants.js
+++
@@ -1,5 +0,0 @@
-const apiConst = (process.env.NODE_ENV === 'production') ? null : 'http://ec2-54-173-199-34.compute-1.amazonaws.com:80/v1/';
-
-export const kGlobalConstants = {
-    API: apiConst
-};
