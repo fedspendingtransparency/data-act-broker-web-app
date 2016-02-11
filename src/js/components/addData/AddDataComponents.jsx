367aace6fcfcb49621bdd8491eecd49649850280
Kyle Fox <kyle.fox55@gmail.com>
2016-02-11 14:27:04 -0500

    User confirmation that we have the file they selected and ready for upload

--- src/js/components/addData/AddDataComponents.jsx
+++ src/js/components/addData/AddDataComponents.jsx
@@ -52,15 +52,32 @@ class FileContainer extends React.Component {
 }
 
 class DropZone extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            fileName: null
+        };
+    }
 
     onDrop(files) {
         this.props.addFileToHolder(files);
+
+        this.setState({
+            filename: files[0].name
+        });
     }
 
     render() {
+        let dropzoneString = 'Drop your file here, or click to select file to upload.';
+
+        if (this.state.filename) {
+            dropzoneString = this.state.filename + ' is ready to be uploaded!';
+        }
+
         return (
             <Dropzone className="text-center" multiple={false} onDrop={this.onDrop.bind(this)}>
-                <div className="center-block usa-da-dropzone">Drop your file here, or click to select file to upload.</div>
+                <div className="center-block usa-da-dropzone">{dropzoneString}</div>
             </Dropzone>
         );
     }
