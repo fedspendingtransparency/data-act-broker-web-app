b63c57688d39635d1cc1298bfc7637a70b42b9b5
Katie Rose <rose_katherine@bah.com>
2016-03-01 20:34:58 -0500

    Showing currently logged in user in navbar

--- src/js/components/SharedComponents/navigation/NavigationComponent.jsx
+++ src/js/components/SharedComponents/navigation/NavigationComponent.jsx
@@ -3,10 +3,41 @@
 * Created by Katie Rose 12/8/15
 **/
 
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { kGlobalConstants } from '../../../GlobalConstants.js';
+import Request from 'superagent';
 import NavbarTab from './NavbarTab.jsx';
 
 export default class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: this.requestReset()
+        };
+    }
+
+
+    requestReset() {
+        Request.get(kGlobalConstants.API + 'current_user/')
+               .withCredentials()
+               .end((err,res) => {
+                   if (err) {
+                       this.setState({
+                           requestSent: true,
+                           resetFailed: true,
+                           user:'none'
+                       });
+                   } else {
+                       this.setState({
+                           requestSent: true,
+                           resetFailed: false,
+                           user: res.body.name
+                       });
+                   }
+               });
+    }
+
     render() {
         const tabNames = {
             'Home': 'landing',
@@ -17,6 +48,7 @@ export default class Navbar extends React.Component {
         };
         const headerTabs = [];
         const context = this;
+        const userText = this.state.user === 'none' ? '' : 'Welcome, ' + this.state.user;
 
         Object.keys(tabNames).map((key) => {
             headerTabs.push(<NavbarTab key={tabNames[key]} name={key} class={tabNames[key]} activeTabClassName={context.props.activeTab} />);
@@ -38,7 +70,7 @@ export default class Navbar extends React.Component {
                 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul id="usa-da-header-link-holder" className="nav navbar-nav navbar-right">
                     {headerTabs}
-                    <li><a className="usa-da-header-link usa-da-user-info" href="#">John Doe</a></li>
+                    <li><a className="usa-da-header-link usa-da-user-info" href="#">{userText}</a></li>
                 </ul>
                 </div>
             </div>
