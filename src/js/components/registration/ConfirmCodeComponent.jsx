e5bd66aeb6967e9daac941ea7fcb2f99fe1d9995
Katie Rose <rose_katherine@bah.com>
2016-03-01 20:10:13 -0500

    refactoring and correcting field validations for registration completion

--- src/js/components/registration/ConfirmCodeComponent.jsx
+++ src/js/components/registration/ConfirmCodeComponent.jsx
@@ -12,6 +12,7 @@ import SubmitButton from '../login/SignInButton.jsx';
 import PasswordComponent from '../login/Password.jsx';
 import ErrorMessage from '../SharedComponents/ErrorMessage.jsx';
 import SuccessMessage from '../SharedComponents/SuccessMessage.jsx';
+import SubmitRegButton from './SubmitButton.jsx';
 
 
 const propTypes = {
@@ -31,7 +32,8 @@ export default class ConfirmCode extends React.Component {
             password1: '',
             password2: '',
             passwordsMatch: true,
-            fieldsComplete: null
+            fieldsComplete: null,
+            buttonDisabled: true
         };
     }
 
@@ -64,21 +66,33 @@ export default class ConfirmCode extends React.Component {
     }
 
     fieldValidation() {
-        if (!this.state.name || !this.state.agency || !this.state.title || !this.state.password1) {
-            if (this.state.requestSent) {
-                this.setState({
-                    fieldsComplete: false
-                });
-            } else {
-                this.setState({
-                    fieldsComplete: false
-                });                    
-            }
-        }
-        else {
+        let fieldEmpty= !this.state.name || !this.state.agency || !this.state.title || !this.state.password1 || !this.state.password2;
+        let passMatch = this.state.password1 === this.state.password2;
+
+        if (fieldEmpty && !passMatch) {
             this.setState({
-                fieldsComplete: true
-            });                
+                fieldsComplete: false,
+                passwordsMatch: false,
+                buttonDisabled: true
+            });                    
+        } else if (fieldEmpty && passMatch) {
+            this.setState({
+                fieldsComplete: false,
+                passwordsMatch: true,
+                buttonDisabled: true
+            });   
+        } else if (!fieldEmpty && !passMatch) {
+            this.setState({
+                fieldsComplete: true,
+                passwordsMatch: false,
+                buttonDisabled: true
+            });               
+        } else {
+            this.setState({
+                fieldsComplete: true,
+                passwordsMatch: true,
+                buttonDisabled: false
+            });  
         }
     }
 
@@ -87,73 +101,51 @@ export default class ConfirmCode extends React.Component {
         if (e.charCode === enterKey) {
             this.requestReset();
             e.preventDefault();
-            this.fieldValidation();
         }
     }
 
-    handleNameChange(e) {
-        this.setState({
-            name: e.target.value
-        });
-        this.fieldValidation();
+    handleFieldChange(e) {
+        switch(e.target.name) {
+            case "regName":
+                this.setState({name: e.target.value}, function() {
+                    this.fieldValidation()
+                });
+            break;
+            case "regTitle":
+                this.setState({title: e.target.value}, function() {
+                    this.fieldValidation()
+                });
+            break;
+            case "regAgency":
+                this.setState({agency: e.target.value}, function() {
+                    this.fieldValidation()
+                });
+            break;
+            case "regPassword1":
+                this.setState({password1: e.target.value}, function() {
+                    this.fieldValidation()
+                });
+            break;
+            case "regPassword2":
+                this.setState({password2: e.target.value}, function() {
+                    this.fieldValidation()
+                });
+            break;
+        }
     }
 
-    handleAgencyChange(e) {
-        this.setState({
-            agency: e.target.value
-        });
-        this.fieldValidation();
-    }
-
-    handleTitleChange(e) {
-        this.setState({
-            title: e.target.value
-        });
-        this.fieldValidation();
-    }
-
-    handlePassword1Change(e) {
-            if (this.state.password2 === e.target.value) {
-                this.setState({
-                    password1: e.target.value,
-                    passwordsMatch: true
-                });                
-            } else {
-                this.setState({
-                    password1: e.target.value,
-                    passwordsMatch: false
-                });                
-            }
-            this.fieldValidation();
-    }
-
-    handlePassword2Change(e) {
-            if (this.state.password1 === e.target.value) {
-                this.setState({
-                    password2: e.target.value,
-                    passwordsMatch: true
-                });                
-            } else {
-                this.setState({
-                    password2: e.target.value,
-                    passwordsMatch: false
-                });                
-            }
-            this.fieldValidation();
-
-    }
     render() {
         let messageComponent = null;
         let passMessageComponent = null;
         let actionComponent = null;
-        let actionButton = <SubmitButton
-                    className="usa-da-button"
-                    onClick={this.requestReset.bind(this)}
-                    buttonText={"Submit"}
-                    />;
+        let actionButton = <SubmitRegButton
+                                onClick={this.requestReset.bind(this)}
+                                buttonText={"Submit"}
+                                buttonDisabled={this.state.buttonDisabled}
+                              />
 
         if (!this.state.fieldsComplete) {
-            actionComponent = <ErrorMessage message={"All fields are required."} />;
+            messageComponent = <ErrorMessage message={"All fields are required."} />;
         }
         if (!this.state.passwordsMatch) {
             passMessageComponent = <ErrorMessage message={"Your passwords do not match."} />;
@@ -175,14 +167,18 @@ export default class ConfirmCode extends React.Component {
                     <Progress totalSteps="4" currentStep="3"/>
                     <h1>Your email has been verified!</h1>
                     <p>Please continue the registration process by providing the following information.</p>
-                    <TextInputComponent inputClass="" inputPlaceholder="Name" inputName="regName" handleChange={this.handleNameChange.bind(this)}/>
-                    <TextInputComponent inputClass="" inputPlaceholder="Agency" inputName="regAgency" handleChange={this.handleAgencyChange.bind(this)} />
-                    <TextInputComponent inputClass="" inputPlaceholder="Title" inputName="regTitle" handleChange={this.handleTitleChange.bind(this)} />
-                    <PasswordComponent handleChange={this.handlePassword1Change.bind(this)}/>
-                    <PasswordComponent handleChange={this.handlePassword2Change.bind(this)}/>
+                    <TextInputComponent inputClass="" inputPlaceholder="Name" inputName="regName" handleChange={this.handleFieldChange.bind(this)}/>
+                    <TextInputComponent inputClass="" inputPlaceholder="Agency" inputName="regAgency" handleChange={this.handleFieldChange.bind(this)} />
+                    <TextInputComponent inputClass="" inputPlaceholder="Title" inputName="regTitle" handleChange={this.handleFieldChange.bind(this)} />
+                    <PasswordComponent fieldID="regPassword1" handleChange={this.handleFieldChange.bind(this)}/>
+                    <PasswordComponent fieldID="regPassword2" handleChange={this.handleFieldChange.bind(this)}/>
                     {passMessageComponent}
-                    {actionComponent}
                     {messageComponent}
+                    <SubmitRegButton
+                                onClick={this.requestReset.bind(this)}
+                                buttonText="Complete Registration"
+                                buttonDisabled={this.state.buttonDisabled}
+                              />
                 </div>
         );
     }
