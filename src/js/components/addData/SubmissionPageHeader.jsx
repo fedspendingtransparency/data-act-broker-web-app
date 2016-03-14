afb98b6ecec3f4e36abc9c67c68595c06ebaaf85
Michael Bray <bray_michael@bah.com>
2016-03-14 14:42:18 -0400

    CSS changes, added docs folder with RSS in it

--- src/js/components/addData/SubmissionPageHeader.jsx
+++ src/js/components/addData/SubmissionPageHeader.jsx
@@ -14,7 +14,8 @@ export default class SubmissionPageHeader extends React.Component {
                         <div className="col-md-6">
                             <h1>Add New Data</h1>
                             <p>The alpha Data Broker allows agencies to test financial data and is not connected to USA Spending.</p>
-                            <p>Upload your files below.</p>
+                            <p>Upload your files below. For more information on the file format,
+                                please <a href="/docs/RSS_v0.7.2.1_AlphaBroker.xlsx" target="_blank">download the Reporting Specification</a>.</p>
                         </div>
                     </div>
                 </div>
