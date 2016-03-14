3019d8cc9fd82e275402d4709166065156525b6e
Michael Bray <bray_michael@bah.com>
2016-03-14 08:50:14 -0400

    Added constants for Local environment, added fonts, removed XBRL submission button

--- GlobalConstants_local.js
+++ GlobalConstants_local.js
@@ -1,6 +1,7 @@
 // Development API path
 export const kGlobalConstants = {
+    //API: 'http://localhost:80/v1/',
     API: 'http://ec2-54-173-199-34.compute-1.amazonaws.com:80/v1/',
     GITHUB: 'https://github.com/fedspendingtransparency/data-act-validator/tree/development',
-    LOCAL: false
+    LOCAL: true
 };
--- src/js/components/addData/AddDataTypeSelector.jsx
+++ src/js/components/addData/AddDataTypeSelector.jsx
@@ -10,11 +10,10 @@ export default class TypeSelector extends React.Component {
         return (
             <div className="usa-da-color-gray-light-half-tone usa-da-login-container usa-grid container">
                 <div className="usa-width-one-whole row center-block">
-                    <h2>Three ways to submit your data...</h2>
+                    <h2>Two ways to submit your data...</h2>
                     <div className="row text-center">
-                        <div className="col-md-4"><button className="usa-da-button-biggest" type="submit" value="Upload CSV Files">Upload CSV Files</button></div>
-                        <div className="col-md-4"><button className="usa-da-button-biggest" type="submit" value="Upload XBRL File" disabled>Upload XBRL File</button></div>
-                        <div className="col-md-4"><button className="usa-da-button-biggest" type="submit" value="Setup a Data Feed" disabled>Setup a Data Feed</button></div>
+                        <div className="col-md-6"><button className="usa-da-button-biggest" type="submit" value="Upload CSV Files">Upload CSV Files</button></div>
+                        <div className="col-md-6"><button className="usa-da-button-biggest" type="submit" value="Setup a Data Feed" disabled>Setup a Data Feed</button></div>
                     </div>
                 </div>
             </div>
