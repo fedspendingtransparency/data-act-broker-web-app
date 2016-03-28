e00247d4a95725102e31e8f5fde3c412fa9eed34
Kevin Li <li_kevin@bah.com>
2016-03-28 11:02:48 -0400

    Remove testing state

--- src/js/components/addData/AddDataPage.jsx
+++ src/js/components/addData/AddDataPage.jsx
@@ -16,21 +16,21 @@ export default class AddDataPage extends React.Component {
     constructor(props) {
         super(props);
 
-        // this.state = {
-        //     agency: "",
-        //     startDate: "",
-        //     endDate: ""
-        // };
-
         this.state = {
-            agency: "Broadcasting Board of Governors",
-            startDate: Moment("2016-01-01", "YYYY-MM-DD"),
-            endDate: Moment("2016-03-15", "YYYY-MM-DD")
-        }
+            agency: "",
+            startDate: "",
+            endDate: ""
+        };
+
+        // this.state = {
+        //     agency: "Broadcasting Board of Governors",
+        //     startDate: Moment("2016-01-01", "YYYY-MM-DD"),
+        //     endDate: Moment("2016-03-15", "YYYY-MM-DD")
+        // }
     }
 
     componentDidMount() {
-        console.error("REMOVE PREPOPULATED STATE BEFORE PUSHING");
+        // console.error("REMOVE PREPOPULATED STATE BEFORE PUSHING");
     }
 
     updateMetaData(newState){
