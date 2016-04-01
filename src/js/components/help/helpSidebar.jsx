6176bc5c97f67b5f79f2a2ce4d911b7ec2d045b0
Michael Bray <bray_michael@bah.com>
2016-04-01 16:02:24 -0400

    Update Help Page CSS per Kaitlin's feedback

--- src/js/components/help/helpSidebar.jsx
+++ src/js/components/help/helpSidebar.jsx
@@ -9,15 +9,19 @@ export default class LandingPage extends React.Component {
     render() {
         return (
             <div className="usa-da-help-sidebar">
-                <h4>
-                    <a href="/#/help#dataElements">Updated Data Elements and Validations</a>
-                </h4>
-                <h4>
-                    <a href="/#/help#accounts">Individual User Accounts</a>
-                </h4>
-                <h4>
-                    <a href="/#/help#browser">Browser Requirements &amp; Known Issues</a>
-                </h4>
+                <h4>What’s New in This Version</h4>
+                <ul>
+                    <li>
+                        <a href="/#/help#dataElements">Updated Data Elements and Validations</a>
+                    </li>
+                    <li>
+                        <a href="/#/help#accounts">Individual User Accounts</a>
+                    </li>
+                    <li>
+                        <a href="/#/help#browser">Browser Requirements &amp; Known Issues</a>
+                    </li>
+                </ul>
+                <h4>Getting More Help</h4>
             </div>
         );
     }
