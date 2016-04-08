b4f9f1f79c7e166a3a3dfbe4ee83bb840f723097
Kevin Li <li_kevin@bah.com>
2016-04-08 12:15:50 -0400

    Updated in-page help links, scroll to section

--- src/js/components/help/helpSidebar.jsx
+++ src/js/components/help/helpSidebar.jsx
@@ -5,20 +5,20 @@
 
 import React from 'react';
 
-export default class LandingPage extends React.Component {
+export default class HelpSidebar extends React.Component {
     render() {
         return (
             <div className="usa-da-help-sidebar">
                 <h6>What’s New in This Version</h6>
                 <ul>
                     <li>
-                        <a href="/#/help#dataElements">Updated Data Elements and Validations</a>
+                        <a href="/#/help?section=dataElements">Updated Data Elements and Validations</a>
                     </li>
                     <li>
-                        <a href="/#/help#accounts">Individual User Accounts</a>
+                        <a href="/#/help?section=accounts">Individual User Accounts</a>
                     </li>
                     <li>
-                        <a href="/#/help#browser">Browser Requirements &amp; Known Issues</a>
+                        <a href="/#/help?section=browser">Browser Requirements &amp; Known Issues</a>
                     </li>
                 </ul>
                 <h6>Getting More Help</h6>
