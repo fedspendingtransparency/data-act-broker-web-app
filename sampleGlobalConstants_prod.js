d92cfb8833d6812881fdccfa77d1005dc3df24b2
Kevin Li <li_kevin@bah.com>
2016-04-12 15:56:46 -0400

    Remove config and replace with sample files

--- sampleGlobalConstants_prod.js
+++ sampleGlobalConstants_prod.js
@@ -3,7 +3,7 @@ export const kGlobalConstants = {
     API: '',
     LOCAL_ROOT : '',
     GITHUB: 'https://github.com/fedspendingtransparency/data-act-validator',
-    GA_TRACKING_ID: 'UA-75081254-1',
+    GA_TRACKING_ID: '',
     LOCAL: false,
     DEV: false,
     BUCKET_NAME: '',
