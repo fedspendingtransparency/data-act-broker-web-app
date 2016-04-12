121e3a796771a4f6ae29df3fa86d8b37518e2fa5
Kevin Li <li_kevin@bah.com>
2016-04-12 15:44:05 -0400

    Remove configuration details from GlobalConstants files

--- GlobalConstants_local.js
+++ GlobalConstants_local.js
@@ -3,9 +3,9 @@ export const kGlobalConstants = {
     API: 'http://localhost:80/v1/',
     LOCAL_ROOT : 'http://localhost:80',
     GITHUB: 'https://github.com/fedspendingtransparency/data-act-validator/tree/development',
-    GA_TRACKING_ID: 'UA-75081254-1',
+    GA_TRACKING_ID: '',
     LOCAL: true,
     DEV: false,
-    BUCKET_NAME: 'dev-data-act-submission',
-    AWS_REGION: 'us-east-1'
+    BUCKET_NAME: '',
+    AWS_REGION: ''
 };
--- GlobalConstants_prod.js
+++ GlobalConstants_prod.js
@@ -1,11 +1,11 @@
 // Production API path
 export const kGlobalConstants = {
-    API: 'http://ec2-54-164-75-75.compute-1.amazonaws.com:80/v1/',
+    API: '',
     LOCAL_ROOT : '',
     GITHUB: 'https://github.com/fedspendingtransparency/data-act-validator',
     GA_TRACKING_ID: 'UA-75081254-1',
     LOCAL: false,
     DEV: false,
-    BUCKET_NAME: 'dev-data-act-submission',
-    AWS_REGION: 'us-gov-west-1'
+    BUCKET_NAME: '',
+    AWS_REGION: ''
 };
