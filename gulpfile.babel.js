b988fbb2e48118f8760416398f8a19a2c8fc41d0
Kevin Li <li_kevin@bah.com>
2016-05-13 16:07:35 -0400

    Rebuilt gulpfile with streamlined output, improved console descriptions, and git commit hash headers in compiled CSS and JS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,28 +5,26 @@ import sass from 'gulp-sass';
 import browserify from 'browserify';
 import watchify from 'watchify';
 import babelify from 'babelify';
-import uglify from 'gulp-uglify';
+import gutil from 'gulp-util';
 import buffer from 'vinyl-buffer';
 import source from 'vinyl-source-stream';
 import sourcemaps from 'gulp-sourcemaps';
-import gutil from 'gulp-util';
+import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
-import replace from 'gulp-replace';
-import gulpif from 'gulp-if';
 import del from 'del';
 import vinylPaths from 'vinyl-paths';
+import replace from 'gulp-replace';
+import gulpif from 'gulp-if';
 import cssNano from 'gulp-cssnano';
-import shell from 'gulp-shell';
-import fs from 'fs';
-import looseEnvify from 'loose-envify';
-
+import merge from 'merge-stream';
+import git from 'gulp-git';
+import header from 'gulp-header';
+import moment from 'moment-timezone';
 import mocha from 'gulp-mocha';
-import babel from 'babel-core/register';
 
 // Base Directories
 const dir = {
     BASE: './',
-    DEV: './dev',
     PUBLIC: './public',
     SRC: './src',
     TEST: './__tests__'
@@ -42,181 +40,271 @@ const path = {
 
     PROD_CONSTANTS: 'GlobalConstants_prod.js',
     DEV_CONSTANTS: 'GlobalConstants_dev.js',
-    LOCAL_CONSTANTS: 'GlobalConstants_local.js',
-    CONSTANTS_DESTNAME: 'GlobalConstants.js',
-    CONSTANTS_DEST: dir.SRC + '/js/',
-
-    CSS_SRC: dir.SRC + '/css/**',
-    CSS_DEV: dir.DEV + '/css',
-    CSS_PUBLIC: dir.PUBLIC + '/css',
-
-    FONTS_SRC: dir.SRC + '/fonts/**',
-    FONTS_DEV: dir.DEV + '/fonts',
-    FONTS_PUBLIC: dir.PUBLIC + '/fonts',
-
-    IMAGES_SRC: dir.SRC + '/img/**',
-    IMAGES_DEV: dir.DEV + '/img',
-    IMAGES_PUBLIC: dir.PUBLIC + '/img',
-
-    GRAPHICS_SRC: dir.SRC + '/graphics/**',
-    GRAPHICS_DEV: dir.DEV + '/graphics',
-    GRAPHICS_PUBLIC: dir.PUBLIC + '/graphics',
-
-    DOCS_SRC: dir.SRC + '/docs/**',
-    DOCS_DEV: dir.DEV + '/docs',
-    DOCS_PUBLIC: dir.PUBLIC + '/docs',
-
-    TEST_RESULTS: dir.TEST + '/test-results',
-
-    NEWRELIC_SRC: dir.SRC + '/newrelic.js',
-    NEWRELIC_DEV: dir.DEV + '/',
-    NEWRELIC_PUBLIC: dir.PUBLIC + '/'
+    LOCAL_CONSTANTS: 'GlobalConstants_local.js'
 };
 
-let build = false;
+let minified = false;
 
-let environmentConstantsEnum = {
-    LOCAL: 0,
-    DEV: 1,
+const environmentTypes = {
+    DEVLOCAL: 0,
+    DEVHOSTED: 1,
     PROD: 2,
-    DEVHOSTED: 3
+    LOCAL: 3
 };
-let environmentConstants = environmentConstantsEnum.DEV;
 
-require('harmonize')();
+const serverDeps = [];
 
-function makeTestFolder() {
-    if (!fs.existsSync(path.TEST_RESULTS)){
-        fs.mkdirSync(path.TEST_RESULTS);
+let commitHash = '';
+const currentTime = moment().tz('America/New_York').format('MMMM D, YYYY h:mm A z');
+
+// set the environment
+let environment = environmentTypes.DEVLOCAL;
+
+gulp.task('setDev', () => {
+    minified = false;
+    environment = environmentTypes.DEVLOCAL;
+    process.env.NODE_ENV = 'development';
+
+    // update the local server dependencies to wait for the compile process to finish before starting
+    serverDeps.push('compile');
+
+    gutil.log('You are running in ' + chalk.black.bgYellow(' DEVELOPMENT (LOCAL) ') + ' mode.');
+});
+
+gulp.task('setDevHosted', () => {
+    minified = false;
+    environment = environmentTypes.DEVHOSTED;
+    process.env.NODE_ENV = 'development';
+
+    gutil.log('You are running in ' + chalk.black.bgYellow(' DEVELOPMENT (HOSTED) ') + ' mode.');
+})
+
+gulp.task('setProd', () => {
+    minified = true;
+    environment = environmentTypes.PROD;
+    process.env.NODE_ENV = 'production';
+
+    gutil.log('You are running in ' + chalk.black.bgGreen(' PRODUCTION (HOSTED) ') + ' mode.');
+});
+
+gulp.task('setLocal', () => {
+    minified = true;
+    environment = environmentTypes.LOCAL;
+    process.env.NODE_ENV = 'production';
+
+    // update the local server dependencies to wait for the compile process to finish before starting
+    serverDeps.push('compile');
+
+    // also have it wait for the minification and HTML modification process
+    serverDeps.push('modifyHtml');
+
+    gutil.log('You are running in ' + chalk.black.bgGreen(' PRODUCTION (LOCAL) ') + ' mode.');
+});
+
+// clear out the existing folder contents
+gulp.task('clean', () => {
+    return del(dir.PUBLIC + '/**/*');
+});
+
+// get the current git metadata
+gulp.task('meta', ['clean'], (done) => {
+    git.revParse({
+        args: '--short HEAD',
+        quiet: true
+    }, (err, hash) => {
+        commitHash = hash;
+        gutil.log('This build is based on commit ' + chalk.cyan(hash) + '.');
+        done();
+    });
+});
+
+// copy the correct constants file into the source directory
+gulp.task('copyConstants', ['meta'], () => {
+
+    let file = path.DEV_CONSTANTS;
+
+    if (environment == environmentTypes.PROD) {
+        file = path.PROD_CONSTANTS;
     }
-}
-
-// TODO: Refactor these 3 tasks to avoid code reuse
-// Copy Fonts to relevant path
-gulp.task('fonts', () => {
-    return gulp.src([path.FONTS_SRC])
-        .pipe(gulp.dest((!build) ? path.FONTS_DEV : path.FONTS_PUBLIC));
-});
-
-// Copy Images to relevant path
-gulp.task('images', () => {
-    return gulp.src([path.IMAGES_SRC])
-        .pipe(gulp.dest((!build) ? path.IMAGES_DEV : path.IMAGES_PUBLIC));
-});
-
-// Copy Graphics to build
-gulp.task('graphics', () => {
-    return gulp.src([path.GRAPHICS_SRC])
-        .pipe(gulp.dest((!build) ? path.GRAPHICS_DEV : path.GRAPHICS_PUBLIC));
-});
-
-gulp.task('docs', () => {
-    return gulp.src([path.DOCS_SRC])
-        .pipe(gulp.dest((!build) ? path.DOCS_DEV : path.DOCS_PUBLIC));
-});
-
-// Use the proper constants file and move to src
-gulp.task('copyConstants', () => {
-
-    let constantsPath = path.PROD_CONSTANTS;
-    if (environmentConstants == environmentConstantsEnum.DEV || environmentConstants == environmentConstantsEnum.DEVHOSTED) {
-        constantsPath = path.DEV_CONSTANTS;
-    }
-    else if (environmentConstants == environmentConstantsEnum.LOCAL) {
-        constantsPath = path.LOCAL_CONSTANTS;
+    else if (environment == environmentTypes.LOCAL) {
+        file = path.LOCAL_CONSTANTS;
     }
 
-    return gulp.src(constantsPath)
-        .pipe(rename(path.CONSTANTS_DESTNAME))
-        .pipe(gulp.dest(path.CONSTANTS_DEST));
+    return gulp.src('./' + file)
+        .pipe(rename('GlobalConstants.js'))
+        .pipe(gulp.dest(dir.SRC + '/js'))
+        .on('end', () => {
+            gutil.log('Using ' + chalk.magenta(file) + ' as the constants file...');
+        });
 });
 
-gulp.task('copyNewRelic', () => {
-    return gulp.src(path.NEWRELIC_SRC)
-        .pipe(gulp.dest((!build) ? path.NEWRELIC_DEV : path.NEWRELIC_PUBLIC));
+// copy the assets
+gulp.task('copyAssets', ['copyConstants'], () => {
+
+    // determine the destination
+    const destination = dir.PUBLIC;
+
+    // combine all the copy streams into a single stream
+    // return the stream to wait for the task to complete
+    return merge(
+        // copy fonts
+        gulp.src(dir.SRC + '/fonts/**/*')
+            .pipe(gulp.dest(dir.PUBLIC + '/fonts'))
+            .on('end', () => {
+                gutil.log('Fonts copied');
+            }),
+
+        // copy graphics
+        gulp.src(dir.SRC + '/graphics/**/*')
+            .pipe(gulp.dest(dir.PUBLIC + '/graphics'))
+            .on('end', () => {
+                gutil.log('Graphics copied');
+            }),
+
+         // copy images
+        gulp.src(dir.SRC + '/img/**/*')
+            .pipe(gulp.dest(dir.PUBLIC + '/img'))
+            .on('end', () => {
+                gutil.log('Images copied');
+            }),
+
+        // copy CSS files
+        gulp.src(dir.SRC + '/css/**/*.css')
+            .pipe(gulp.dest(dir.PUBLIC + '/css'))
+            .on('end', () => {
+                gutil.log('CSS copied');
+            }),
+
+        // copy the main index file
+        gulp.src('./index.html')
+            .pipe(gulp.dest(dir.PUBLIC))
+            .on('end', () => {
+                gutil.log('index.html copied');
+            }),
+
+        // copy newrelic.js
+        gulp.src(dir.SRC + '/newrelic.js')
+            .pipe(gulp.dest(dir.PUBLIC))
+            .on('end', () => {
+                gutil.log('newrelic.js copied');
+            })
+
+    );
 });
 
-// Compile react files with Browserify and watch
-// for changes with Watchify
-gulp.task('watch', ['fonts', 'images', 'graphics', 'docs', 'copyConstants', 'copyNewRelic'], () => {
-    const props = {
+// build the SASS
+gulp.task('sass', ['copyAssets'], () => {
+
+    if (environment == environmentTypes.DEVLOCAL) {
+        // set up a watcher for future SASS changes
+        gulp.watch([dir.SRC + '/css/**/*.scss', dir.SRC + '/_scss/**/*.scss'])
+            .on('change', () => {
+                gutil.log(chalk.green('Starting SASS recompile...'));
+                return gulp.src(dir.SRC + '/css/**/*.scss')
+                    .pipe(sass.sync().on('error', sass.logError))
+                    .pipe(gulp.dest(dir.PUBLIC + '/css'))
+                    // auto reload the browser
+                    .pipe(connect.reload())
+                    .on('end', () => {
+                        gutil.log(chalk.green('SASS compiled.'));
+                    });
+            });
+    }
+
+    // compile SASS files
+    return gulp.src(dir.SRC + '/css/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        // add in the commit hash and timestamp header
+        .pipe(header('/* Build ' + commitHash  + '\n' + currentTime + ' */\n\n'))
+        // .pipe(gulpif(minified, cssNano()))
+        .pipe(gulp.dest(dir.PUBLIC + '/css'));
+});
+
+// convert the React JSX into normal JS
+gulp.task('build', ['sass'], () => {
+    const buildProps = {
         entries: [path.ENTRY_POINT],
-        transform: [[babelify, { presets: ['es2015', 'react'] }, {envify: { global: true}}]],
-        debug: !build,
-        cache: {}, packageCache: {}, fullPaths: true
+        transform: [
+            [
+                babelify, 
+                { 
+                    presets: ['es2015', 'react']
+                },
+                {
+                    envify: {
+                        global: true 
+                    }
+                }
+            ]
+        ],
+        debug: !minified,
+        cache: {},
+        packageCache: {},
+        fullPaths: true
     };
-    let watcher = browserify(props);
-    if (environmentConstants == environmentConstantsEnum.DEV) {
-        watcher = watchify(browserify(props));
-    }
 
-    return watcher.on('update', () => {
-        watcher.bundle()
-            .on('error', gutil.log)
+    let buildTool = browserify(buildProps);
+
+    // use a watcher if we're serving the file in local dev instead
+    if (environment == environmentTypes.DEVLOCAL) {
+        buildTool = watchify(browserify(buildProps));
+
+        // set up watcher for recompiling
+        buildTool.on('update', () => {
+            gutil.log(chalk.cyan('Starting JS recompile...'));
+            buildTool.bundle().on('error', gutil.log)
             .pipe(source(path.OUT))
             .pipe(buffer())
-            .pipe(sourcemaps.init({ loadMaps: !build }))
-            .pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest((!build) ? dir.DEV : dir.PUBLIC))
+            .pipe(sourcemaps.init({ loadMaps: !minified }))
+            .pipe(gulpif(!minified, sourcemaps.write('.')))
+            .pipe(gulp.dest(dir.PUBLIC))
+            // auto reload the browser
+            .pipe(connect.reload())
             .on('end', () => {
                 gutil.log(chalk.cyan('Reloading JS...'));
-            })
-            .pipe(connect.reload());
-    })
+            });
+        });
+    }
+
+    return buildTool
         .bundle().on('error', gutil.log)
         .pipe(source(path.OUT))
         .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: !build }))
-        .pipe(gulpif(!build, sourcemaps.write('.')))
-        .pipe(gulp.dest((!build) ? dir.DEV : dir.PUBLIC));
-});
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', () => {
-    return gulp.src(path.CSS_SRC)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulpif(build, cssNano()))
-        .pipe(gulp.dest((!build) ? path.CSS_DEV : path.CSS_PUBLIC))
-        .pipe(connect.reload());
-});
-
-// HTML replace scripts with minified version for build
-// Move index.html to public folder for deployment
-gulp.task('html-replace', () => {
-
-    // don't replace with minified files unless not in local dev
-    if (environmentConstants == environmentConstantsEnum.DEV) {
-        return gulp.src([path.INDEX_SRC])
-            .pipe(gulp.dest(dir.PUBLIC));
-    }
-    else {
-        return gulp.src([path.INDEX_SRC])
-            .pipe(replace(path.OUT, path.MINIFIED_OUT))
-            .pipe(gulp.dest(dir.PUBLIC));
-    }
-
-
-    
-});
-
-// Minifying
-gulp.task('minify', ['sass', 'watch', 'html-replace'], () => {
-    return gulp.src([dir.PUBLIC + '/' + path.OUT])
-        .pipe(vinylPaths(del))
-        .pipe(rename(path.MINIFIED_OUT))
-        .pipe(uglify())
+        .pipe(sourcemaps.init({ loadMaps: !minified }))
+        .pipe(gulpif(!minified, sourcemaps.write('.')))
+        // add in the commit hash and timestamp header
+        .pipe(header('/* Build ' + commitHash  + '\n' + currentTime + ' */\n\n'))
         .pipe(gulp.dest(dir.PUBLIC));
 });
 
-gulp.task('reload', () => {
-    connect.reload();
+
+// minify the JS
+gulp.task('minify', ['build'], () => {
+
+    return gulp.src(dir.PUBLIC + '/' + path.OUT)
+        // delete the non-minified file
+        .pipe(vinylPaths(del))
+        // set the minified output
+        .pipe(rename(path.MINIFIED_OUT))
+        // minify
+        .pipe(uglify())
+        // add in the commit hash and timestamp header (which would have been removed by the minification process)
+        .pipe(header('/* Build ' + commitHash  + '\n' + currentTime + ' */\n\n'))
+        // write out the output
+        .pipe(gulp.dest(dir.PUBLIC));
+
+});
+
+// after minifying, change the HTML script tag to point to the minified file
+gulp.task('modifyHtml', ['minify'], () => {
+    return gulp.src(dir.PUBLIC + '/index.html')
+        .pipe(replace(path.OUT, path.MINIFIED_OUT))
+        .pipe(gulp.dest(dir.PUBLIC));
 });
 
 
-gulp.task('serve', ['set-build', 'html-replace', 'sass', 'watch'], () => {
-    gulp.watch([path.SASS_SRC, path.CSS_SRC], ['sass']);
-    gulp.watch(['*.html'], ['reload']);
-
+// serve the frontend locally
+gulp.task('serve', serverDeps, () => {
     connect.server({
         root: dir.PUBLIC,
         port: 3000,
@@ -224,53 +312,40 @@ gulp.task('serve', ['set-build', 'html-replace', 'sass', 'watch'], () => {
     });
 });
 
-// Set build variable for conditional build tasks
-gulp.task('set-build', () => {
-    build = true;
+// convenience task that does all the copying and compiling subtasks
+gulp.task('compile', ['clean', 'meta', 'copyConstants', 'copyAssets', 'sass', 'build']);
+
+// user-initiated gulp tasks
+gulp.task('buildDev', ['setDevHosted', 'compile'], () => {
+    
 });
 
-gulp.task('set-hosted-dev', () => {
-    environmentConstants = environmentConstantsEnum.DEVHOSTED;
-})
-
-// Set constants variable for production constants file
-gulp.task('set-prod-constants', () => {
-    environmentConstants = environmentConstantsEnum.PROD;
+gulp.task('dev', ['setDev', 'compile', 'serve'], () => {
+    
 });
 
-// Set constants variable for local constants file
-gulp.task('set-local-constants', () => {
-    environmentConstants = environmentConstantsEnum.LOCAL;
+gulp.task('buildLocal', ['setDevHosted', 'compile'], () => {
+
 });
 
-// Production task
-gulp.task('production', ['set-build', 'set-prod-constants', 'minify']);
+gulp.task('local', ['setLocal', 'compile', 'minify', 'modifyHtml', 'compile', 'serve'], () => {
 
-// Build with dev constants for demos task
-gulp.task('buildDev', ['set-build', 'set-hosted-dev', 'minify']);
-
-// Build with local constants for local deployment
-gulp.task('buildLocal', ['set-build', 'set-local-constants', 'minify']);
-
-// Default task for development
-gulp.task('default', ['serve']);
-
-gulp.task('test-results', () => {
-    makeTestFolder();
 });
 
-gulp.task('jest', shell.task('jest', {
-    // So our task doesn't error out when a test fails
-    ignoreErrors: true
-}));
+gulp.task('production', ['setProd', 'compile', 'minify', 'modifyHtml'], () => {
+    
+});
 
-gulp.task('test', ['test-results', 'jest']);
+gulp.task('default', ['local']);
 
+
+
+// run unit tests
 gulp.task('expresso', () => {
     return gulp.src(['./__unittests__/**/*-spec.js','./__unittests__/**/*-spec.jsx', '!./__unittests__/support/*.js'], { read: false })
         .pipe(mocha({
             compilers: {
-                js: babel
+                js: require('babel-core/register')
             },
             require: ['./__unittests__/setup.js']
         }))
@@ -280,7 +355,7 @@ gulp.task('mocha', () => {
     return gulp.src(['./__unittests__/**/*-spec.js','./__unittests__/**/*-spec.jsx', '!./__unittests__/support/*.js'], { read: false })
         .pipe(mocha({
             compilers: {
-                js: babel
+                js: require('babel-core/register')
             },
             require: ['./__unittests__/setup.js'],
             reporter: 'mocha-junit-reporter',
