9bf294972b20809677a249a113ffdce37bef2b56
Kevin Li <li_kevin@bah.com>
2016-05-26 14:56:32 -0400

    Help page changelog content and sidebar are now populated from a Markdown file

--- src/js/components/help/helpPage.jsx
+++ src/js/components/help/helpPage.jsx
@@ -9,7 +9,35 @@ import HelpSidebar from './helpSidebar.jsx';
 import HelpContent from './helpContent.jsx';
 import Footer from '../SharedComponents/FooterComponent.jsx';
 
+import * as HelpHelper from '../../helpers/helpHelper.js';
+
 export default class HelpPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            changelog: '',
+            sections: []
+        };
+    }
+
+    componentDidMount() {
+        this.loadChangelog();
+    }
+
+    loadChangelog() {
+        HelpHelper.loadChangelog()
+            .then((output) => {
+                console.log(output);
+                this.setState({
+                    changelog: output.html,
+                    sections: output.sections
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 
     render() {
         return (
@@ -28,10 +56,10 @@ export default class HelpPage extends React.Component {
                     <div className="container">
                         <div className="row usa-da-help-page">
                             <div className="col-md-4">
-                                <HelpSidebar />
+                                <HelpSidebar sections={this.state.sections} />
                             </div>
                             <div className="col-md-8">
-                                <HelpContent section={this.props.location.query.section} />
+                                <HelpContent section={this.props.location.query.section} changelog={this.state.changelog} />
                             </div>
                         </div>
                     </div>
--- src/js/components/help/helpSidebar.jsx
+++ src/js/components/help/helpSidebar.jsx
@@ -4,28 +4,19 @@
  **/
 
 import React from 'react';
+import HelpSidebarItem from './helpSidebarItem.jsx';
 
 export default class HelpSidebar extends React.Component {
     render() {
+        const sectionList = this.props.sections.map((section, index) => {
+            return <HelpSidebarItem key={index} sectionName={section.name} sectionId={section.link} />
+        });
+
         return (
             <div className="usa-da-help-sidebar">
                 <h6>What’s New in This Release</h6>
                 <ul>
-                    <li>
-                        <a href="/#/help?section=brokerIE">Logging into the Broker with Internet Explorer</a>
-                    </li>
-                    <li>
-                        <a href="/#/help?section=pipe">Submit Files with Pipe Symbol</a>
-                    </li>
-                    <li>
-                        <a href="/#/help?section=fileValv1">File Validations per RSS v1.0</a>
-                    </li>
-                    <li>
-                        <a href="/#/help?section=crossFileValv1">Cross File Validations</a>
-                    </li>
-                    <li>
-                        <a href="/#/help?section=browser">Browser Requirements &amp; Known Issues</a>
-                    </li>
+                    {sectionList}
                 </ul>
                 <h6>Getting More Help</h6>
                 <ul>
