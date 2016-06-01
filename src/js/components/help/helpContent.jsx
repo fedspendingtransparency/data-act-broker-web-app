c5e72817e62efd62ed0349ae789f9ade107cf6e5
Kaitlin Devine <kaitlin.devine@gsa.gov>
2016-06-01 12:31:27 -0400

    new help page

--- src/js/components/help/helpContent.jsx
+++ src/js/components/help/helpContent.jsx
@@ -55,9 +55,138 @@ export default class HelpContent extends React.Component {
     render() {
         return (
             <div className="usa-da-help-content">
-                <p>Welcome to the DATA Act Broker – Alpha Release. This release is a <a href="https://en.wikipedia.org/wiki/Minimum_viable_product" target="_blank">Minimum Viable Product</a> and represents just enough functionality so that we can gather critical user feedback to determine the direction and implementation of future features. This version of the Broker aligns with <a href={this.state.rssUrl} target="_blank">Reporting Submission Specification (RSS v1.0)</a>.</p>
+                <p>Welcome to the DATA Act Broker Alpha Release. This release is a <a href="https://en.wikipedia.org/wiki/Minimum_viable_product" target="_blank">Minimum Viable Product</a> and represents just enough functionality so that we can gather critical user feedback to determine the direction and implementation of future features. This version of the Broker aligns with <a href={this.state.rssUrl} target="_blank">Reporting Submission Specification (RSS v1.0)</a>.</p>
 
-                <h2>What's New in This Release</h2>
+           <h2>What's New in This Release</h2>
+           <h4>June 1, 2016</h4>
+        
+                <p>In this version of the broker, we have added some information on the screens to help you with your data submission, added some functionality to help you select the reporting period, and updated some of the validations.</p>
+                
+                <ul>
+                    <li>
+                        <a href="/#/help?section=stepby">Step-by-Step Guide on the Broker Home Page</a>
+                    </li>
+                    <li>
+                        <a href="/#/help?section=subguide">Submission Guide</a>
+                    </li>
+                    <li>
+                        <a href="/#/help?section=dateselect">Select Reporting Period</a>
+                    </li>
+                     <li>
+                        <a href="/#/help?section=valjun01">Updated Validations</a>
+                    </li>
+              
+                    <li>
+                        <a href="/#/help?section=browser">Browser Requirements &amp; Known Issues</a>
+                    </li>
+                </ul>
+
+                <h4 name="stepby">Step-by-Step Guide on the Broker Home Page</h4>
+                <p>When you log into the Broker, you will see three choices that guide you to <em>Upload and validate a new submission</em>, <em>Continue with an existing submission</em>, and <em>Review, certify, and publish a submission.</em></p>
+        
+        <h4 name="subguide">Submission Guide</h4>
+                <p>The Submission Guide provides details of the four steps to submit your agency's data. Once you have reviewed this page, you can check a box to hide this page the next time you log into the Broker.</p>
+        
+        <h4 name="dateselect">Select Reporting Period</h4>
+                <p>Based on user feedback, the quarterly submission dates are displayed as the quarter number and the fiscal year. Example: Quarter 2 - 2016.</p>
+        
+        <h4 name="valjun01">Updated Validations</h4>
+                <p>These validation rules have been implemented:</p>
+
+ <table className="usa-da-table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Relates to File</th>
+      <th scope="col">Rule Name</th>
+      <th scope="col">Rule Detail</th>
+      <th scope="col">Notes</th>
+    </tr>
+  </thead>
+   
+<tr><td>A</td>
+<td>A2</td>
+<td>BudgetAuthorityAvailableAmountTotal_CPE = BudgetAuthorityAppropriatedAmount_CPE + BudgetAuthorityUnobligatedBalanceBroughtForward_FYB + AdjustmentsToUnobligatedBalanceBroughtForward_CPE + OtherBudgetaryResourcesAmount_CPE</td>
+<td>Calculation</td>
+</tr>
+<tr><td>A</td>
+<td>A3</td>
+<td>OtherBudgetaryResourcesAmount_CPE = ContractAuthorityAmountTotal_CPE + BorrowingAuthorityAmountTotal_CPE + SpendingAuthorityfromOffsettingCollectionsAmountTotal_CPE</td>
+<td>Calculation</td>
+</tr>
+<tr><td>A</td>
+<td>A4</td>
+<td>StatusOfBudgetaryResourcesTotal_CPE= ObligationsIncurredTotalByTAS_CPE + UnobligatedBalance_CPE</td>
+<td>Calculation</td>
+</tr>
+<tr><td>A</td>
+<td>A24</td>
+<td>StatusOfBudgetaryResourcesTotal_CPE = BudgetAuthorityAvailableAmountTotal_CPE</td>
+<td>In File A, both of these elements must match, per TAS</td>
+</tr>
+<tr><td>B</td>
+<td>B3</td>
+<td>ObligationsUndeliveredOrdersUnpaidTotal (FYB or CPE) = USSGL(4801 + 4831 + 4871 + 4881). This applies to the program activity and object class level.</td>
+<td>Calculation</td>
+</tr>
+<tr><td>B</td>
+<td>B4</td>
+<td>ObligationsDeliveredOrdersUnpaidTotal (FYB or CPE) = USSGL(4901 + 4931 + 4971 + 4981). This applies to the program activity and object class level.</td>
+<td>Calculation</td>
+</tr>
+<tr><td>B</td>
+<td>B5</td>
+<td>GrossOutlayAmountByProgramObjectClass (FYB or CPE, File B) = GrossOutlaysUndeliveredOrdersPrepaidTotal (FYB or CPE, File B) + GrossOutlaysDeliveredOrdersPaidTotal (FYB or CPE, File B)</td>
+<td>Calculation</td>
+</tr>
+<tr><td>B</td>
+<td>B6</td>
+<td>GrossOutlaysUndeliveredOrdersPrepaidTotal (FYB or CPE) = USSGL(4802 + 4832 + 4872+ 4882). This applies to the program activity and object class level.</td>
+<td>Calculation</td>
+</tr>
+<tr><td>B</td>
+<td>B7</td>
+<td>GrossOutlaysDeliveredOrdersPaidTotal (FYB or CPE)= USSGL(4902 + 4908 + 4972 + 4982). This applies to the program activity and object class level.</td>
+<td>Calculation</td>
+</tr>
+<tr><td>B</td>
+<td>B13</td>
+<td>DeobligationsRecoveriesRefundsdOfPriorYearByProgramObjectClass_CPE = USSGL(4871+ 4872 + 4971 + 4972)</td>
+<td>Calculation</td>
+</tr>
+<tr><td>C</td>
+<td>C3</td>
+<td>ObligationsUndeliveredOrdersUnpaidTotal (FYB or CPE) = USSGL(4801 + 4831 + 4871 + 4881). This applies to the award level.</td>
+<td>Calculation</td>
+</tr>
+<tr><td>C</td>
+<td>C4</td>
+<td>ObligationsDeliveredOrdersUnpaidTotal (FYB or CPE) = USSGL(4901 + 4931 + 4971 + 4981). This applies to the award level.</td>
+<td>Calculation</td>
+</tr>
+<tr><td>C</td>
+<td>C5</td>
+<td>GrossOutlayAmountByAward (FYB or CPE, File C) = GrossOutlaysUndeliveredOrdersPrepaidTotal (FYB or CPE, File C) + GrossOutlaysDeliveredOrdersPaidTotal (FYB or CPE, File C)</td>
+<td>Calculation</td>
+</tr>
+<tr><td>C</td>
+<td>C6</td>
+<td>GrossOutlaysUndeliveredOrdersPrepaidTotal (FYB or CPE) = USSGL(4802 + 4832 + 4872+ 4882). This applies to the award level.</td>
+<td>Calculation</td>
+</tr>
+<tr><td>C</td>
+<td>C7</td>
+<td>GrossOutlaysDeliveredOrdersPaidTotal (FYB or CPE) = USSGL(4902 + 4908 + 4972 + 4982). This applies to the award level.</td>
+<td>Calculation</td>
+</tr>
+<tr><td>C</td>
+<td>C17</td>
+<td>Not Required on rows that include values for USSGL account balances or subtotals.</td>
+<td>This is for transactions, not for USSGL amounts, from the financial systems.</td>
+</tr>   
+  </table>
+                                
+
+<h4 name="may172016">What's New in This Release - May 17, 2016</h4>
         
                 <p>In this version of the broker, we have made a change if you are logging in with Internet Explorer, added funtionality for the Broker to recognize files with the pipe symbol as a delimiter, and updated some of the validations.</p>
                 
