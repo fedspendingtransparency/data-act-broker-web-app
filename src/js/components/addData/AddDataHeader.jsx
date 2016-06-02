1fb28dc5d2d730f642884746c3a236b4318841e2
Michael Bray <bray_michael@bah.com>
2016-06-02 16:32:05 -0400

    Initial check in of Last Updated feature (waiting on backend to update date to UTC timestamp; will also add Moment functionality afterwards)

--- src/js/components/addData/AddDataHeader.jsx
+++ src/js/components/addData/AddDataHeader.jsx
@@ -1,56 +1,66 @@
 /**
-* SubmissionPageHeader.jsx
+* AddDataHeader.jsx
 * Created by Kyle Fox 2/19/16
 **/
 
 import React from 'react';
-import { generateRSSUrl } from '../../helpers/util.js';
+import moment from 'moment';
+
+import * as ReviewHelper from '../../helpers/reviewHelper.js';
+
+class LastUpdated extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div className="col-md-5 mt-40 mb-50">
+                Last Saved: {this.props.last_updated}
+            </div>
+        );
+    }
+}
 
 export default class AddDataHeader extends React.Component {
     constructor(props) {
         super(props);
 
-        this.rssPromise = null;
-
         this.state = {
-            rssUrl: ''
+            submissionID: null,
+            last_updated: null,
+            ready: false
         };
     }
 
     componentDidMount() {
-        this.rssPromise = generateRSSUrl();
-        this.rssPromise.promise
-            .then((url) => {
-                this.setState({
-                    rssUrl: url
+        if (this.props.submissionID != null) {
+            ReviewHelper.fetchStatus(this.props.submissionID)
+                .then((data) => {
+                    data.ready = true;
+                    this.setState(data);
+                })
+                .catch((error) => {
+                    console.log(error);
                 });
-
-                this.rssPromise = null;
-            });
-    }
-
-    componentWillUnmount() {
-        if (this.rssPromise) {
-            this.rssPromise.cancel();
         }
     }
 
     render() {
+        let lastUpdated = null;
+        if (this.state.ready && this.state.last_updated){
+            lastUpdated = <LastUpdated last_updated={this.state.last_updated} />
+        }
+
         return (
             <div className="usa-da-content-dark">
                 <div className="container">
                     <div className="row usa-da-content-add-data usa-da-page-title">
-                        <div className="col-md-7 mt-40 mb-50">
+                        <div className="col-md-7 mt-40 mb-20">
                             <div className="display-2">Add New Data</div>
-                            <p>The current DATA Act Broker allows agencies to upload and test their data as it is required under the DATA Act. It is not connected to USAspending. To get started, you will need the following files:
-                                    <li>A: Appropriation Data (<em><a href="https://s3-us-gov-west-1.amazonaws.com/prod-data-act-web-static-files/sample-files/appropValid.csv" target="_blank">Sample file</a></em>)</li>
-                                    <li>B: Program Activity and Object Class Data (<em><a href="https://s3-us-gov-west-1.amazonaws.com/prod-data-act-web-static-files/sample-files/programActivityValid.csv" target="_blank">Sample file</a></em>)</li>
-                                    <li>C: Award Financial Data (<em><a href="https://s3-us-gov-west-1.amazonaws.com/prod-data-act-web-static-files/sample-files/awardFinancialValid.csv" target="_blank">Sample file</a></em>)</li>
-                                    <li>D2: Financial Assistance Award File (<em><a href="https://s3-us-gov-west-1.amazonaws.com/prod-data-act-web-static-files/sample-files/awardValid.csv" target="_blank">Sample file</a></em>) <br />*<em style={{fontSize: '.8em'}}>The D2 file will be generated for users in future broker versions. Agencies must still upload a D2 file currently. If you do not have this data, simply submit the sample file.</em></li>
-
-                            </p>
-                            <p>Upload your files below. For more information on the file format, please <a href={this.state.rssUrl} target="_blank">download the Reporting Submission Specification (Draft RSS 1.0)</a>.</p>
+                            <p>The current DATA Act Broker allows agencies to upload and test their data as it is required under the DATA Act. It is not connected to USAspending.</p>
                         </div>
+                        {lastUpdated}
                     </div>
                 </div>
             </div>
