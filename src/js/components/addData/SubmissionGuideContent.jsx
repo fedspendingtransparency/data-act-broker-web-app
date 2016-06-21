1c15918d232f6a99cb188a35dcad7d07c1dff308
Kevin Li <li_kevin@bah.com>
2016-06-21 14:43:38 -0400

    Link to list of current validations

--- src/js/components/addData/SubmissionGuideContent.jsx
+++ src/js/components/addData/SubmissionGuideContent.jsx
@@ -93,6 +93,7 @@ export default class SubmissionGuideContent extends React.Component {
                                         <div className="col-md-11 usa-da-reg-wrapper mt-20">
                                             <h4>Validate Data Files</h4>
                                             <p>In this step, the Data Broker will validate the files separately and against each other. All files must be present to perform cross-file validations.</p>
+                                            <p><a href="#/help?section=updatedValidations">Find out what validations are currently implemented</a></p>
                                         </div>
                                     </div>
                                 </div>
