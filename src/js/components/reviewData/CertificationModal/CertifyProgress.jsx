172bb5b0c4c10357657c0b3d619a4d3d2282ce43
Kevin Li <li_kevin@bah.com>
2016-09-08 12:11:58 -0400

    Implement certify modal

---
+++ src/js/components/reviewData/CertificationModal/CertifyProgress.jsx
@@ -0,0 +1,143 @@
+/**
+  * CertifyProgress.jsx
+  * Created by Kevin Li 9/7/16
+  **/
+
+import React from 'react';
+import FileProgress from '../../SharedComponents/FileProgress.jsx';
+import * as Icons from '../../SharedComponents/icons/Icons.jsx';
+
+const defaultProps = {
+	finished: false,
+	apiTimeTotal: 1000,
+	apiTimeCount: 1
+};
+
+export default class CertifyProgress extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.isUnmounted = false;
+
+		this.state = {
+			progress: 0,
+			unit: (1/6),
+			subiteration: 1,
+			iteration: 0,
+			averageDuration: 0,
+			hasError: false,
+			errorMessage: ""
+		}
+	}
+
+	componentDidMount() {
+		this.isUnmounted = false;
+		this.startProgress();
+	}
+
+	componentWillUnmount() {
+		this.isUnmounted = true;
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevProps.finished != this.props.finished) {
+			if (this.props.finished) {
+				this.setState({
+					progress: 100
+				});
+			}
+		}
+	}
+
+	startProgress() {
+		// calculate the average duration of an API call in ms
+		const averageDuration = this.props.apiTimeTotal / this.props.apiTimeCount;
+		this.setState({
+			progress: 0,
+			averageDuration: averageDuration
+		}, () => {
+			this.predictedProgress();
+		});
+
+	}
+
+	predictedProgress() {
+		// make an update every 1/6 of the average duration
+		window.setTimeout(() => {
+
+			let unit = this.state.unit;
+			let subiteration = this.state.subiteration + 1;
+			let iteration = this.state.iteration;
+			let newProgress = this.state.progress + unit;
+
+			if (newProgress >= 0.96) {
+				newProgress = 0.96;
+			}
+
+			if (iteration > 4) {
+				subiteration = 1;
+				unit = (1 - newProgress)/6;
+				iteration++;
+			}
+
+			if (!this.isUnmounted && this.state.progress < 100) {
+				this.setState({
+					progress: newProgress,
+					subiteration: subiteration,
+					unit: unit,
+					iteration: iteration
+				}, () => {
+
+					// just give up after 10 full iterations
+					if (iteration <= 10) {
+						this.predictedProgress();
+					}
+				});
+			}
+
+		}, this.state.averageDuration/6);
+
+	}
+
+
+	render() {
+
+		let hideClass = " hide";
+		let hideError = " hide";
+		if (this.state.hasError) {
+			hideError = "";
+		}
+		else if (this.state.progress == 100) {
+			hideClass = "";
+		}
+
+		return (
+			<div className="certify-progress">
+				<FileProgress fileStatus={this.state.progress * 100} />
+				<div className={"completed-content" + hideClass}>
+					<div className="success-wrapper">
+						<div className="success-header">
+							Complete!
+						</div>
+						<div className="success-message">
+							Your submission has been published to USAspending.gov and will appear on the site within 24 hours.
+						</div>
+					</div>
+					<button className="usa-da-button close-button" onClick={this.props.closeModal}>
+						Close
+					</button>
+				</div>
+
+				<div className={"alert alert-danger" + hideError}>
+					<span className="usa-da-icon"><Icons.ExclamationCircle /></span>
+                    <div className="alert-header-text">Publish Failed</div>
+                    <p>An error occurred while attempting to publish this submission.</p>
+                    <p>{this.state.errorMessage}</p>
+				</div>
+			</div>
+		)
+	}
+}
+
+CertifyProgress.defaultProps = defaultProps;
---
+++ src/js/components/reviewData/CertificationModal/ReviewDataCertifyModal.jsx
@@ -0,0 +1,136 @@
+/**
+  * ReviewDataCertifyModal.jsx
+  * Created by Kevin Li 9/6/16
+  **/
+
+import React from 'react';
+import Modal from 'react-aria-modal';
+import * as Icons from '../../SharedComponents/icons/Icons.jsx';
+
+import CertifyDisclaimer from './CertifyDisclaimer.jsx';
+import CertifyButtons from './CertifyButtons.jsx';
+import CertifyProgress from './CertifyProgress.jsx';
+
+class VariableMessage extends React.Component {
+	render() {
+		let message = "1 warning";
+		if (this.props.warnings != 1) {
+			message = this.props.warnings + " warnings";
+		}
+
+		return (
+			<h6>
+				This submission contains <span className="variable-field">{message}</span>.
+			</h6>
+		)
+	}
+}
+
+export default class ReviewDataCertifyModal extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			certified: false,
+			showProgress: false,
+			publishComplete: false,
+			closeable: true
+		};
+	}
+
+	clickedCertifyCheckbox(e) {
+		this.setState({
+			certified: !this.state.certified
+		});
+	}
+
+	clickedCertifyButton(e) {
+		e.preventDefault();
+
+		// show the progress bar
+		this.setState({
+			showProgress: true,
+			closeable: false
+		}, () => {
+			// mock the API completion
+			window.setTimeout(() => {
+				this.setState({
+					publishComplete: true,
+					closeable: true
+				});
+			}, 5 * 1000);
+		});
+	}
+
+	closeModal(e) {
+		if (e) {
+			e.preventDefault();
+		}
+
+		if (!this.state.closeable) {
+			return;
+		}
+
+		// reset the modal if closed
+		this.setState({
+			showProgress: false,
+			certified: false,
+			publishComplete: false
+		}, () => {
+			this.props.closeModal();
+		});
+	}
+
+	render() {
+		let message = null;
+		if (this.props.warnings > 0) {
+			message = <VariableMessage warnings={this.props.warnings} />;
+		}
+
+		let action = <CertifyButtons {...this.props}
+						certified={this.state.certified}
+						clickedCertifyButton={this.clickedCertifyButton.bind(this)}
+						clickedCertifyCheckbox={this.clickedCertifyCheckbox.bind(this)} />;
+
+		if (this.state.showProgress) {
+			action = <CertifyProgress {...this.props.session} 
+						finished={this.state.publishComplete} 
+						closeModal={this.closeModal.bind(this)} />;
+		}
+
+		let hideClose = "";
+		if (!this.state.closeable) {
+			hideClose = " hide";
+		}
+
+		return (
+			<Modal mounted={this.props.isOpen} onExit={this.closeModal.bind(this)} underlayClickExits={this.state.closeable}
+				verticallyCenter={true} initialFocus="#certify-check" titleId="usa-da-certify-modal">
+				<div className="usa-da-modal-page">
+					<div id="usa-da-certify-modal" className="usa-da-certify-modal">
+                        <div className={"usa-da-certify-modal-close usa-da-icon usa-da-icon-times" + hideClose}>
+                            <a href="#" onClick={this.closeModal.bind(this)}> <Icons.Times /> </a>
+                        </div>
+
+                        <div className="usa-da-certify-modal-content">
+                        	<div className="row">
+	                            <div className="col-md-12 title-field">
+	                                <h6>Are you sure you want to publish your data?</h6>
+	                                {message}
+	                            </div>
+	                        </div>
+	                        <div className="row">
+	                        	<div className="col-md-12">
+		                        	<CertifyDisclaimer />
+		                        </div>
+	                        </div>
+
+	                        {action}
+
+                        </div>
+                    </div>
+				</div>
+			</Modal>
+		)
+	}
+}
