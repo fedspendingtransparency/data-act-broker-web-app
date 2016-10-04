fee575f28e362efd35db8a7c99272d252e19b459
Mike Bray <mab6bf@virginia.edu>
2016-10-04 11:45:14 -0400

    Update helpNav.jsx

--- src/js/components/help/helpNav.jsx
+++ src/js/components/help/helpNav.jsx
@@ -14,23 +14,23 @@ export default class HelpNav extends React.Component {
         super(props);
     }
 
+    render() {
+        const pageLinks = this.props.pageArray.map((page) => {
+	    let url = "/#/" + page.toLowerCase();
 
-
-	render() {
-		const pageLinks = this.props.pageArray.map((page) => {
-			let url = "/#/" + page.toLowerCase();
-
-								if(this.props.selected == page){
-									 return <a href={url} className="selected usa-da-button btn-lg">{page}</a>;
-								} else {
-								 	 return <a href={url} className="usa-da-button btn-lg">{page}</a>;
-								}
-					      });
-	    return (
-	      <div className="help-nav">
-	                  {pageLinks}
-	      </div>
-	    );
-	}
+	    if(this.props.selected == page){
+	        return <a href={url} className="selected usa-da-button btn-lg">{page}</a>;
+	    } else {
+	        return <a href={url} className="usa-da-button btn-lg">{page}</a>;
+	    }
+        });
+		
+        return (
+	    <div className="help-nav">
+	        {pageLinks}
+	    </div>
+	);
+    }
 }
+
 HelpNav.defaultProps = defaultProps;
