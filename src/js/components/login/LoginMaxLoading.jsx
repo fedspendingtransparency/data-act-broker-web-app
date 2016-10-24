6f47943b77e6ea167dfade823ec72242e45f0d2f
Mike Bray <mab6bf@virginia.edu>
2016-10-24 08:51:35 -0400

    Update LoginMaxLoading.jsx

--- src/js/components/login/LoginMaxLoading.jsx
+++ src/js/components/login/LoginMaxLoading.jsx
@@ -9,10 +9,10 @@ import ErrorMessage from '../SharedComponents/ErrorMessage.jsx';
 import LoadingBauble from '../SharedComponents/overlays/LoadingBauble.jsx';
 
 export default class LoginMaxLoading extends React.Component {
-	render() {
-		let errorMessageComponent = null;
-		let hideLoading = '';
-		let hideError = ' hide';
+    render() {
+        let errorMessageComponent = null;
+        let hideLoading = '';
+        let hideError = ' hide';
 
         if (this.props.errorMessage && this.props.errorMessage != "") {
             errorMessageComponent = <ErrorMessage message={this.props.errorMessage} />;
@@ -20,28 +20,27 @@ export default class LoginMaxLoading extends React.Component {
             hideError = '';
         }
 
-		return (
+        return (
             <div>
                 <div className="row">
-                	<div className={"col-md-12 login-loading" + hideLoading}>
-                		<div className="loading-animation">
-	                        <LoadingBauble />
-	                    </div>
-	                    <div className="loading-text">
-	                    	Signing into the DATA Act Broker...
-	                    </div>
+                    <div className={"col-md-12 login-loading" + hideLoading}>
+                        <div className="loading-animation">
+                            <LoadingBauble />
+                        </div>
+                        <div className="loading-text">
+                            Signing into the DATA Act Broker...
+                        </div>
                     </div>
-                        
                 </div>
                 <div className={"row" + hideError}>
                     <div className="col-md-12">
                         {errorMessageComponent}
                         <div className="back-link">
-                    		<a href="#">Back to login page</a>
+                            <a href="#">Back to login page</a>
                         </div>
                     </div>
                 </div>
             </div>
-		)
-	}
+        )
+    }
 }
