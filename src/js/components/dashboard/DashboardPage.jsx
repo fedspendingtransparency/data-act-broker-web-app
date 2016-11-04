1bef94909728275e7c9231882eac4efc124a0af4
Kevin Li <li_kevin@bah.com>
2016-11-04 15:39:15 -0400

    Fix tabbing

--- src/js/components/dashboard/DashboardPage.jsx
+++ src/js/components/dashboard/DashboardPage.jsx
@@ -11,23 +11,23 @@ import DashboardContainer from '../../containers/dashboard/DashboardContainer.js
 export default class DashboardPage extends React.Component {
 	render() {
 		return (
-			<div>
-            	<div className="usa-da-site_wrap usa-da-dashboard-page">
-	                <Navbar activeTab="dashboard"/>
-	                <div className="usa-da-content-dark">
+            <div>
+                <div className="usa-da-site_wrap usa-da-dashboard-page">
+                    <Navbar activeTab="dashboard"/>
+                    <div className="usa-da-content-dark">
                         <div className="container">
                             <div className="row">
                                 <div className="col-md-12 mt-40 mb-20">
                                     <div className="display-2" data-contentstart="start" tabIndex={-1}>
-										Submission Dashboard
-									</div>
+                                        Submission Dashboard
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <DashboardContainer />
-	            </div>
-	            <Footer />
+                </div>
+                <Footer />
             </div>
 		)
 	}
