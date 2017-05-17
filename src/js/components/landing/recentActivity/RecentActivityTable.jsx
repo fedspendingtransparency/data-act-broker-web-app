03d0a197a2dd14fa0acca11a143cc8516b3d2c57
Minahm Kim <kim_minahm@bah.com>
2017-05-17 10:58:06 -0400

    extra console.log

--- src/js/components/landing/recentActivity/RecentActivityTable.jsx
+++ src/js/components/landing/recentActivity/RecentActivityTable.jsx
@@ -19,8 +19,6 @@ export default class RecentActivityTable extends React.Component {
 	constructor(props) {
 		super(props);
 
-		console.log(this.props)
-
 		this.didUnmount = false;
 
 		this.state = {
