353b3e08008408c0c56c33deb58829a8d120b202
Minahm Kim <kim_minahm@bah.com>
2017-06-13 12:34:59 -0400

    fix for warning during upload

--- src/js/components/addData/AddDataContent.jsx
+++ src/js/components/addData/AddDataContent.jsx
@@ -7,6 +7,8 @@ import React, { PropTypes } from 'react';
 import { kGlobalConstants } from '../../GlobalConstants.js';
 import SubmissionComponent from './SubmissionComponent.jsx';
 import SubmitButton from '../SharedComponents/SubmitButton.jsx';
+import * as Icons from '../../components/SharedComponents/icons/Icons.jsx';
+import * as ReviewHelper from '../../helpers/reviewHelper.js';
 
 const propTypes = {
     metaData: PropTypes.object
@@ -19,9 +21,22 @@ export default class AddDataContent extends React.Component {
         this.state = {
             fileHolder: [],
             submissionID: 0,
-            progress: 0
+            progress: 0, 
+            upload: false
         };
     }
+
+    componentWillUnmount(){
+        if((this.props.submission.state == 'uploading' || this.props.submission.state == 'failed') && this.props.submission.id !== null){
+            ReviewHelper.failUpload(this.props.submission.id)
+        }
+    }
+ 
+    startUpload(){
+        this.setState({upload: true});
+        this.props.performUpload();
+    }
+ 
 
     render() {
 
@@ -36,7 +51,7 @@ export default class AddDataContent extends React.Component {
         let actionArea = "";
         const submissionState = this.props.submission.state;
         if (submissionState == 'ready' || submissionState == 'failed') {
-            actionArea = <SubmitButton onClick={this.props.performUpload} className="usa-da-button-bigger btn-primary" buttonText="Upload & Validate CSV files" testId="upload" />;
+            actionArea = <SubmitButton onClick={this.startUpload.bind(this)} className="usa-da-button-bigger btn-primary" buttonText="Upload & Validate CSV files" testId="upload" />;
         }
         else if (submissionState == 'uploading') {
             actionArea = <SubmitButton className="usa-da-button-bigger" buttonText="Uploading files..." buttonDisabled={true} />;
@@ -45,8 +60,30 @@ export default class AddDataContent extends React.Component {
             actionArea = <SubmitButton className="usa-da-button-bigger" buttonText="Upload & Validate CSV files" buttonDisabled={true} />;
         }
 
+        let warning = null;
+         
+        if(this.props.submission.state === 'failed'){
+            warning = <div className='container short'>
+                        <div className={"alert alert-error text-left"} role="alert">
+                            <span className="usa-da-icon error-icon"><Icons.ExclamationCircle /></span>
+                            <div className="alert-header-text">Your upload has failed</div>
+                            <p>Please try again. If the problem persists, contact the service desk.</p>
+                        </div>
+                    </div>;
+        }else if(this.state.upload){
+            warning = <div className='container short'>
+                        <div className={"alert alert-error text-left"} role="alert">
+                            <span className="usa-da-icon error-icon"><Icons.ExclamationCircle /></span>
+                            <div className="alert-header-text">Your files are uploading</div>
+                            <p>Please stay on this page until they're complete or your submission may not be created properly.</p>
+                        </div>
+                    </div>;
+        }
+
+
         return (
             <div>
+            {warning}
                 <div className="container center-block">
                     <div className="row">
                         <SubmissionComponent files={this.props.fileTypes} />
