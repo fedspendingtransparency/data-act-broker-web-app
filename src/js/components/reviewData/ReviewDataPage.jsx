aa3cd58fc5472fcfbcdf7bba3193714c2e684383
Minahm Kim <minahmkim@C02J94G3DKQ5.fios-router.home>
2017-07-11 12:55:03 -0400

    repeated check

--- src/js/components/reviewData/ReviewDataPage.jsx
+++ src/js/components/reviewData/ReviewDataPage.jsx
@@ -32,7 +32,7 @@ export default class ReviewDataPage extends React.Component {
         }
 
         let gtasWarning = null;
-        if(this.props.data.gtas && this.props.data.gtas) {
+        if(this.props.data.gtas) {
             gtasWarning = <GTASBanner data={this.props.data.gtas}/>
         }
 
