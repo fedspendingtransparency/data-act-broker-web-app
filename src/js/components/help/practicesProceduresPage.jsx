b427107f0c7d2336baa76b24b9a0a9e4c5dae882
Minahm Kim <mdkim105@gmail.com>
2017-07-24 10:53:48 -0400

    fixing links in the practices and procedures page

--- src/js/components/help/practicesProceduresPage.jsx
+++ src/js/components/help/practicesProceduresPage.jsx
@@ -16,37 +16,39 @@ export default class PracticesProceduresPage extends React.Component {
     }
 
     render() {
-        return (
-          <div className="usa-da-help-style-page" name="top">
-              <div className="usa-da-page-content">
-                  <Navbar activeTab="help" logoOnly={this.props.helpOnly} type={this.props.type}/>
-                  <div className="usa-da-content-dark mb-60">
-                      <div className="container">
-                          <div className="row">
-                              <div className="col-md-12 mt-40 mb-20">
-                                  <div className="display-2" data-contentstart="start" tabIndex={-1}>Help | DATA Act Broker</div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="container">
-                      <div className="row usa-da-help-page">
-                            <div className="col-md-12">
-                                <PracticesProceduresContent />
+      const practices = this.props.type === 'fabs' ? '#/detachedPractices' : '#/practices';
+      let color = this.props.type === 'fabs' ? 'teal' : 'dark';
+      return (
+        <div className="usa-da-help-style-page" name="top">
+            <div className="usa-da-page-content">
+                <Navbar activeTab="help" logoOnly={this.props.helpOnly} type={this.props.type}/>
+                <div className={"usa-da-content-" + color + " mb-60"}>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12 mt-40 mb-20">
+                                <div className="display-2" data-contentstart="start" tabIndex={-1}>Help | DATA Act Broker</div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row usa-da-help-page">
+                          <div className="col-md-12">
+                              <PracticesProceduresContent />
                           </div>
-                      </div>
-                  </div>
-                  <Footer />
-                  <div className="usa-da-help-top-button">
-                      <a href="#/practices?section=top" aria-label="Back to top">
-                          <div className="usa-da-icon">
-                              <Icons.AngleUp alt="Arrow pointing up" />
-                          </div>
-                          <span className="hidden-label">Back to top</span>
-                      </a>
-                  </div>
-              </div>
-        );
-    }
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+                <div className="usa-da-help-top-button">
+                    <a href={practices+"?section=top"} aria-label="Back to top">
+                        <div className="usa-da-icon">
+                            <Icons.AngleUp alt="Arrow pointing up" />
+                        </div>
+                        <span className="hidden-label">Back to top</span>
+                    </a>
+                </div>
+            </div>
+      );
+  }
 }
