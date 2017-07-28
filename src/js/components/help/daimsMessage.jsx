b12dd555159b6144b309574acd8d1e12a2c72435
kimminahm <mdkim105@gmail.com>
2017-07-28 15:32:33 -0400

    Update daimsMessage.jsx
    Caps

--- src/js/components/help/daimsMessage.jsx
+++ src/js/components/help/daimsMessage.jsx
@@ -12,7 +12,7 @@ export default class DaimsMessage extends React.Component {
 
     render() {
         let type = <span>.</span>;
-        let version = 'v1.1.1'
+        let version = 'v1.1.1';
         if(this.props.type == 'validations') {
             type = <span> and its <a href='https://community.max.gov/download/attachments/1286474850/DAIMS_Validation_Rules_v1.1.xlsx?version=1&modificationDate=1498857363500&api=v2' className='daims-link'>Validations {version}.</a></span>
         } else if(this.props.type == 'practices') {
@@ -24,7 +24,7 @@ export default class DaimsMessage extends React.Component {
                 <span className='daims-header'>June 30, 2017: </span>
                 <span className='daims-message'>
                     Treasury released the DATA Act Information Model Schema (DAIMS) {version}.
-                    Daims {version} is a minor update of the schema and will be implemented in production in the fall of 2017.
+                    DAIMS {version} is a minor update of the schema and will be implemented in production in the fall of 2017.
                     Find out more information about <a className='daims-link' href='https://community.max.gov/x/YgyuT'>DAIMS {version}</a>{type}
                 </span>
             </div>
