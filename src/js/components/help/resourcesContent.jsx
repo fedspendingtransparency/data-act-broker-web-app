6541f526e1a416f223dfe3b04fef61bf682a6ab6
Minahm Kim <mdkim105@gmail.com>
2017-09-18 16:57:24 -0400

    DAIMS v1.1.2 help page changes. FABS has some more customized help page content

--- src/js/components/help/resourcesContent.jsx
+++ src/js/components/help/resourcesContent.jsx
@@ -51,7 +51,8 @@ export default class ResourcesContent extends React.Component {
 					schemaFileAUrl: urls['DATA_Act_Schema_v1_0_File_A_20160622.pdf'],
 					schemaFileBUrl: urls['DATA_Act_Schema_v1_0_File_B_20160622.pdf'],
 					schemaFileCUrl: urls['DATA_Act_Schema_v1_0_File_C_20160622.pdf'],
-					faqFileC: urls['DATA_Act_PMO_FileC_TOA_FAQ_20160913.pdf']
+					faqFileC: urls['DATA_Act_PMO_FileC_TOA_FAQ_20160913.pdf'],
+					fabs_sample: urls['DAIMS_FABS_Sample_Submission_File_v1.1.csv']
 				});
 
 				this.urlPromise = null;
@@ -85,93 +86,130 @@ export default class ResourcesContent extends React.Component {
 	}
 
 	render() {
-		let validations = this.props.type === 'fabs' ? '/#/FABSValidations' : '/#/validations';
-		return (
-			<div className="usa-da-help-content">
-				<div className="resources-page">
-				<h2>Resources DAIMS</h2>
-					<DaimsMessage type='resources' />
-					<p>The DATA Act information Model Schema (DAIMS) gives an overall view of the hundreds of distinct data elements used to tell the story of how federal dollars are spent. It includes artifacts that provide technical guidance for federal agencies about what data to report to Treasury including the authoritative sources of the data elements and the submission format.</p>
-					<p>Click these links to download or open a file.</p>
-
-					<h5>Reporting Submission Specification (RSS) Resources</h5>
+		let content = <div>
+					<h5>Here are some resources to assist with your submission to FABS</h5>
+					<ul>
+						<li>
+							FABS File: Financial Assistance data (Award and Awardee Attributes) <a href={this.state.fabs_sample} target='_blank'>Download sample file</a>
+						</li>
+					</ul>
+					<h5>Additional Resources</h5>
+					<ul>
+						<li>
+							<a href="/#/FABSvalidations">FABS Validations page</a>
+						</li>
+						<li>
+							<a href='/#/resources'>DATA Act Information Model Schema (DAIMS)</a> resources related to FABS including:
+							<ul>
+								<li>
+									<a href='https://community.max.gov/download/attachments/1286474850/DAIMS_Practices_Procedures_v1.1.pdf?api=v2'>DAIMS Practices and Procedures v1.1</a>
+								</li>
+								<li>
+									<a href='http://fedspendingtransparency.github.io/assets/docs/DAIMS_IDD_v1.1.xlsx'>DAIMS IDD v1.1 (D2 tab)</a>
+								</li>
+								<li>
+									<a href='http://fedspendingtransparency.github.io/assets/docs/DAIMS_Domain_Values_v1.1.xlsx'>DAIMS Domain Values v1.1</a>
+								</li>
+								<li>
+									<a href='http://fedspendingtransparency.github.io/assets/docs/DAIMS_Information_Flow_Diagram_v1.1.png'>Information Flow Diagram</a> - An overview of the reporting cadences and sources of data included in the DAIMS.
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>;
+			if (this.props.type == 'dabs') {
+				content = <div><h5>Reporting Submission Specification (RSS) Resources</h5>
 					<p>The RSS is a listing of the data elements with specific instructions for submitting content in the appropriate format.</p>
 					 <ul>
-										<li>
-										<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_RSS_v1.1.xlsx">RSS v1.1</a>
-										</li>
-										<li><a href={this.state.faqFileC}>TransactionObligatedAmount FAQs</a>
-										</li>
+						<li>
+							<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_RSS_v1.1.xlsx">RSS v1.1</a>
+						</li>
+							<li><a href={this.state.faqFileC}>TransactionObligatedAmount FAQs</a>
+						</li>
 					</ul>
 					<h6>RSS Sample Files</h6>
 					<ul>
-										<li>
-											File A: Appropriation Account data&nbsp;
-											<a href="https://s3-us-gov-west-1.amazonaws.com/prod-data-act-web-static-files/sample-files/appropValid.csv" target="_blank" rel="noopener noreferrer">Download sample file</a>
-										</li>
-										<li>
-											File B: Object Class and Program Activity data&nbsp;
-											<a href="https://s3-us-gov-west-1.amazonaws.com/prod-data-act-web-static-files/sample-files/programActivityValid.csv" target="_blank" rel="noopener noreferrer">Download sample file</a>
-										</li>
-										<li>
-											File C: Award Financial data&nbsp;
-											<a href="https://s3-us-gov-west-1.amazonaws.com/prod-data-act-web-static-files/sample-files/awardFinancialValid.csv" target="_blank" rel="noopener noreferrer">Download sample file</a>
-										</li>
+						<li>
+							File A: Appropriation Account data&nbsp;
+							<a href="https://s3-us-gov-west-1.amazonaws.com/prod-data-act-web-static-files/sample-files/appropValid.csv" target="_blank" rel="noopener noreferrer">Download sample file</a>
+						</li>
+						<li>
+							File B: Object Class and Program Activity data&nbsp;
+							<a href="https://s3-us-gov-west-1.amazonaws.com/prod-data-act-web-static-files/sample-files/programActivityValid.csv" target="_blank" rel="noopener noreferrer">Download sample file</a>
+						</li>
+						<li>
+							File C: Award Financial data&nbsp;
+							<a href="https://s3-us-gov-west-1.amazonaws.com/prod-data-act-web-static-files/sample-files/awardFinancialValid.csv" target="_blank" rel="noopener noreferrer">Download sample file</a>
+						</li>
 					</ul>
 					<h6>DAIMS Diagrams for RSS</h6>
 					<p>These are visual representations of how the different data elements are related. They show the groupings of elements and attributes.</p>
 					<ul>
-					<li>
-					<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_RSS_Diagram_File_A_v1.1.pdf" target="_blank" rel="noopener noreferrer">File A - Appropriation Account Detail</a>
-					</li>
-					<li>
-					<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_RSS_Diagram_File_B_v1.1.pdf" target="_blank" rel="noopener noreferrer">File B - Object Class and Program Activity Detail</a>
-					</li>
-					<li>
-					<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_RSS_Diagram_File_C_v1.1.pdf" target="_blank" rel="noopener noreferrer">File C - Award Financial Detail</a>
-					</li>
+						<li>
+							<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_RSS_Diagram_File_A_v1.1.pdf" target="_blank" rel="noopener noreferrer">File A - Appropriation Account Detail</a>
+						</li>
+						<li>
+							<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_RSS_Diagram_File_B_v1.1.pdf" target="_blank" rel="noopener noreferrer">File B - Object Class and Program Activity Detail</a>
+						</li>
+						<li>
+							<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_RSS_Diagram_File_C_v1.1.pdf" target="_blank" rel="noopener noreferrer">File C - Award Financial Detail</a>
+						</li>
 					</ul>
 					<h5>Interface Definition Document (IDD) Resources</h5>
 					<p>The IDD is listing of the data elements that will be extracted from government-wide procurement and financial assistance systems.</p>
 					<ul>
-										<li>
-										<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_IDD_v1.1.xlsx">IDD v1.1</a>
-										</li>
+						<li>
+							<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_IDD_v1.1.xlsx">IDD v1.1</a>
+						</li>
 					</ul>
 					<h6>DAIMS Diagrams for IDD</h6>
 					<p>These are visual representations of how the different data elements are related. They show the groupings of elements and attributes.</p>
 					<ul>
-					<li>
-					<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_IDD_Diagram_File_D1_v1.1.pdf" target="_blank" rel="noopener noreferrer">File D1 - Award and Awardee Attributes (Procurement)</a>
-					</li>
-					<li>
-					<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_IDD_Diagram_File_D2_v1.1.pdf" target="_blank" rel="noopener noreferrer">File D2 - Award and Awardee Attributes (Financial Assistance)</a>
-					</li>
-					<li>
-					<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_IDD_Diagram_File_E_v1.1.pdf" target="_blank" rel="noopener noreferrer">File E - Additional Awardee Attributes</a>
-					</li>
-					<li>
-					<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_IDD_Diagram_File_F_v1.1.pdf" target="_blank" rel="noopener noreferrer">File F - Sub-Award Attributes</a>
-					</li>
+						<li>
+							<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_IDD_Diagram_File_D1_v1.1.pdf" target="_blank" rel="noopener noreferrer">File D1 - Award and Awardee Attributes (Procurement)</a>
+						</li>
+						<li>
+							<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_IDD_Diagram_File_D2_v1.1.pdf" target="_blank" rel="noopener noreferrer">File D2 - Award and Awardee Attributes (Financial Assistance)</a>
+						</li>
+						<li>
+							<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_IDD_Diagram_File_E_v1.1.pdf" target="_blank" rel="noopener noreferrer">File E - Additional Awardee Attributes</a>
+						</li>
+						<li>
+							<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_IDD_Diagram_File_F_v1.1.pdf" target="_blank" rel="noopener noreferrer">File F - Sub-Award Attributes</a>
+						</li>
 					</ul>
 					<h5>Overall DAIMS Resources</h5>
 					<ul>
-					<li>
-					<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_Information_Flow_Diagram_v1.1.png" target="_blank" rel="noopener noreferrer">Information Flow Diagram</a> - An overview of the reporting cadences and sources of data included in the DAIMS.
-					</li>
-					<li>
-					<a href="https://community.max.gov/download/attachments/1286474850/DAIMS_Practices_Procedures_v1.1.pdf?api=v2" target="_blank" rel="noopener noreferrer">Practices and Procedures page</a>
-					</li>
-					<li>
-					<a href={validations} target="_blank" rel="noopener noreferrer">Validation Rules page</a>
-					</li>
-					<li>
-					<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_Domain_Values_v1.1.xlsx">Domain Values</a> - A listing of the specific set of allowed values for a data element.
-					</li>
-					<li>
-					<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_Agency_Label_To_Terse_Label_v1.1.xlsx">Long Element Name to Short Element Name Crosswalk</a> - A listing of the shortened column names for the data elements in the RSS and IDD.
-					</li>
+						<li>
+							<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_Information_Flow_Diagram_v1.1.png" target="_blank" rel="noopener noreferrer">Information Flow Diagram</a> - An overview of the reporting cadences and sources of data included in the DAIMS.
+						</li>
+						<li>
+							<a href="https://community.max.gov/download/attachments/1286474850/DAIMS_Practices_Procedures_v1.1.pdf?api=v2" target="_blank" rel="noopener noreferrer">Practices and Procedures page</a>
+						</li>
+						<li>
+							<a href='/#/FABSValidations' target="_blank" rel="noopener noreferrer">FABS Validation Rules page</a>
+						</li>
+						<li>
+							<a href="/#/FABSresources" target="_blank" rel="noopener noreferrer">FABS Resources Page</a>
+						</li>
+						<li>
+							<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_Domain_Values_v1.1.xlsx">Domain Values</a> - A listing of the specific set of allowed values for a data element.
+						</li>
+						<li>
+							<a href="http://fedspendingtransparency.github.io/assets/docs/DAIMS_Agency_Label_To_Terse_Label_v1.1.xlsx">Long Element Name to Short Element Name Crosswalk</a> - A listing of the shortened column names for the data elements in the RSS and IDD.
+						</li>
 					</ul>
+				</div>
+			}
+		return (
+			<div className="usa-da-help-content">
+				<div className="resources-page">
+				<h2>Resources {this.props.type}</h2>
+					<DaimsMessage type='resources' />
+					<p>The DATA Act information Model Schema (DAIMS) gives an overall view of the hundreds of distinct data elements used to tell the story of how federal dollars are spent. It includes artifacts that provide technical guidance for federal agencies about what data to report to Treasury including the authoritative sources of the data elements and the submission format.</p>
+					<p>Click these links to download or open a file.</p>
+
+					{content}
 			</div>
 			</div>
 		);
