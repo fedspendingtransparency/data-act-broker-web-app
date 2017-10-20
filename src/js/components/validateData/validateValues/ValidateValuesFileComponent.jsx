7d70e6a1416304410b14867d2aef370be758c7b1
Minahm Kim <mdkim105@gmail.com>
2017-10-20 11:00:10 -0400

    missed console log

--- src/js/components/validateData/validateValues/ValidateValuesFileComponent.jsx
+++ src/js/components/validateData/validateValues/ValidateValuesFileComponent.jsx
@@ -219,7 +219,6 @@ export default class ValidateValuesFileComponent extends React.Component {
         }
 
         let buttonOverlay = '', validationElement = '', isOptional = false, uploadText = 'Choose Corrected File';
-        console.log(this.props, this.state.hasErrors, this.state.hasWarnings)
         if ((this.props.published === 'unpublished' || !this.props.published) && permission) {
             // user has permissions and submission is not published
             if (this.state.hasErrors) {
