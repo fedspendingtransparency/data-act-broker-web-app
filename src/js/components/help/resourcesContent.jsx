07eb1ecc0725cd994c2ba9fab5f52c55b3351210
Lizzie Salita <salita_elizabeth@bah.com>
2018-12-12 14:32:27 -0500

    Fixed SQL validation rules link; updated spacing

--- src/js/components/help/resourcesContent.jsx
+++ src/js/components/help/resourcesContent.jsx
@@ -80,19 +80,18 @@ export default class ResourcesContent extends React.Component {
                             href="https://fiscal.treasury.gov/files/data-transparency/DAIMS-Validation-Rules-v1.2.1.xlsx"
                             target="_blank"
                             rel="noopener noreferrer">
-                            Validation Rules:
+                            Validation Rules
                         </a>
-                        &nbsp;documents the business rules employed by the DATA Act Broker for field
+                        : documents the business rules employed by the DATA Act Broker for field
                         and cross-file validations.
                     </li>
                     <li>
                         <a
-                            href="https://github.com/fedspendingtransparency/data-act-broker-backend/
-                            tree/master/dataactvalidator/config/sqlrules"
+                            href="https://github.com/fedspendingtransparency/data-act-broker-backend/tree/master/dataactvalidator/config/sqlrules"
                             target="_blank"
-                            rel="noopener noreferrer">SQL Validation Rules:
+                            rel="noopener noreferrer">SQL Validation Rules
                         </a>
-                        &nbsp;the actual machine logic used in FABS validations.
+                        : the actual machine logic used in FABS validations.
                         These SQL statements are based directly on the FABS validation rules.
                     </li>
                 </ul>
@@ -116,12 +115,12 @@ export default class ResourcesContent extends React.Component {
                         hundreds of distinct data elements used to tell the story of how federal dollars are spent.
                         It includes artifacts that provide technical guidance for federal agencies about what data to
                         report to Treasury including the authoritative sources of the data elements and the submission
-                        format. DAIMS information is available on the
+                        format. DAIMS information is available on the&nbsp;
                         <a
                             href="https://www.fiscal.treasury.gov/data-transparency/data-act-v1.2.html"
                             target="_blank"
                             rel="noopener noreferrer">
-                            &nbsp;DAIMS
+                            DAIMS
                         </a>
                         &nbsp;page of the Data Transparency site for the Bureau of the Fiscal Service.
                     </span>
