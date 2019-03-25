26035655e70ea73ea3a8e9fd4fcc27990f6da6da
Jonathan Hill <hill_jonathan@bah.com>
2019-03-25 14:37:48 -0400

    add classnames back

--- src/js/components/dashboard/DashboardTable.jsx
+++ src/js/components/dashboard/DashboardTable.jsx
@@ -4,6 +4,7 @@
 //   */
 
 import React, { PropTypes } from 'react';
+import cx from 'classnames';
 import _ from 'lodash';
 
 import FormattedTable from '../SharedComponents/table/FormattedTable';
@@ -374,7 +375,10 @@ export default class DashboardTable extends React.Component {
                 changePage={this.changePage.bind(this)} />);
         }
 
-        const loadingClass = this.props.isLoading ? 'loading' : '';
+        const tableHeaderClasses = cx({
+            'submission-table-content': true,
+            loading: this.props.isLoading || this.state.noResults
+        });
 
         const headers = this.getHeaders();
         // cannot be added to the const because if a user is read only then delete will not be created
@@ -388,7 +392,7 @@ export default class DashboardTable extends React.Component {
 
         return (
             <div className="usa-da-submission-list">
-                <div className={`submission-table-content ${loadingClass}`}>
+                <div className={tableHeaderClasses}>
                     <FormattedTable
                         headers={headers}
                         data={this.state.parsedData}
