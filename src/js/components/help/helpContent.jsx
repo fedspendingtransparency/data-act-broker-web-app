f5ca6ffbcea088f8bd00d92d6ce508377a475d64
Daniel Boos <boos_daniel@bah.com>
2019-04-09 10:18:30 -0400

    Release Notes - April 5, 2019

--- src/js/components/help/helpContent.jsx
+++ src/js/components/help/helpContent.jsx
@@ -85,28 +85,29 @@ export default class HelpContent extends React.Component {
             <div className="usa-da-help-content">
 
                 <h2 className="usa-da-help-content__header">What&#8217;s New in This Release</h2>
-                <strong className="usa-da-help-content__date">March 25, 2019</strong>
+                <strong className="usa-da-help-content__date">April 5, 2019</strong>
 
                 <p className="usa-da-help-content__content">In this release of the Broker, we:</p>
                 <ul className="usa-da-help-content__list">
-                    <li>Made edits to File F generation to align with changes in DAIMS v1.3.1</li>
-                    <li>Implemented the FABS38.2.2 validation rule from DAIMS v1.3.1.</li>
-                    <li>Renamed FABS38.2 to FABS38.2.1 to accommodate the new FABS38.2.2 validation rule.</li>
-                    <li>Implemented the FABS38.4.1 validation rule from DAIMS v1.3.1</li>
-                    <li>Renamed FABS38.4 to FABS38.4.1 to accommodate the new FABS38.4.2 validation rule. </li>
-                    <li>Updated B9 Validation Rule to be case-insensitive when checking the program activity name/program activity code combination for the corresponding funding TAS/TAF.</li>
-                    <li>Fixed aberrant display box behavior that occasionally occurred when viewing with the visual summary information on errors or warnings for a submission.</li>
+                    <li>Updated Broker Resource page to reference DAIMS v1.3.1 documents.</li>
+                    <li>Implemented DAIMS v1.3.1 changes to elements in File D1 and D2. Users who have scripts based on D1/D2 files should update them to match the D1/D2 elements and names in DAIMS v1.3.1.</li>
+                    <li>Updated Submission Dashboard to notify user if there are no submissions to display.</li>
+                    <li>Fixed edge-case issue with email sent after the creation of the DABS submission. These errors only occurred for agencies associated with a FREC. </li>
+                    <li>Fixed small issue with line count inaccuracies in File A that occurred when extra carriage returns were present in the submission file.</li>
+                    <li>Fixed validation rule A34 to correctly generate a critical error when the BudgetAuthorityUnobligatedBalanceBroughtForward value is empty.</li>
+                    <li>Fixed download URL expiration issue for File A/D1/D2. Links are now generated upon click.</li>
+                    <li>Various backend changes and improvements surrounding job handling and memory management.</li>
                 </ul>
 
                 <h2 className="usa-da-help-content__header">Technical Notes for this Release</h2>
-                <strong className="usa-da-help-content__date">March 25, 2019</strong>
+                <strong className="usa-da-help-content__date">April 5, 2019</strong>
 
                 <p className="usa-da-help-content__content">In this release, here is a list of technical changes that may require infrastructure or database updates, or represents additional functionality.</p>
                 <ul className="usa-da-help-content__list">
-                    <li>Updated certify_submission API endpoint documents to include new errors for standard quarterly revalidation thresholds and special revalidation thresholds. This rule checks the validation date and confirms the submission is certifiable.</li>
-                    <li>Modified the TAS loader to prevent loading any possible duplicates.</li>
-                    <li>Standardized unique award keys between Broker and USASpending.gov.</li>
-                    <li>Optimized File F generation for performance in memory and speed.</li>
+                    <li>Updated unique award keys in transaction data to be consistent with USAspending.gov.</li>
+                    <li>Updated the CFDA loader to pull from a common source shared with USAspending.gov.</li>
+                    <li>Removed deprecated submit_detached_file endpoint from the API. This should not affect Broker Inbound API users given that they do not use this endpoint.</li>
+                    <li>Fixed the logic of providing current fiscal quarter on the frontend.</li>
                 </ul>
                 <h2 className="usa-da-help-content-subheading">Getting More Help</h2>
 
