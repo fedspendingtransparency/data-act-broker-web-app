ccd508078370ad4606678a54a503843983eee179
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-05-20 17:24:04 -0400

    [migrate-to-webpack4] env specific configs

---
+++ webpack.config.dev.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.config.common');
+
+module.exports = merge(common, {
+    mode: "development",
+    devtool: "eval",
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "public"),
+        host: "0.0.0.0", // this allows VMs to access the server
+        port: 3000,
+        disableHostCheck: true
+    }
+});
