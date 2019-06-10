9803794c66933a2baedc855cf857b714718cd89b
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-06-10 15:37:41 -0400

    [webpack] adding NormalModuleReplacementPlugin

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const GitRevisionPlugin = require("git-revision-webpack-plugin");
 
-const isProduction = (process.env.NODE_ENV === 'production');
+const isProduction = process.env.NODE_ENV === "production";
 
 const gitRevisionPlugin = new GitRevisionPlugin({ branch: true }); // 'rev-parse HEAD' is default command to find latest commit
 
@@ -13,7 +13,7 @@ console.log("Commit Hash for this build: ", gitRevisionPlugin.commithash());
 console.log("Branch for this build: ", gitRevisionPlugin.branch());
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/js/app'),
+    entry: path.resolve(__dirname, "../src/js/app"),
     context: path.resolve(__dirname, "../src"),
     output: {
         // https://webpack.js.org/guides/caching/
@@ -22,7 +22,7 @@ module.exports = {
         path: path.resolve(__dirname, "../public")
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: [".js", ".jsx"],
         modules: ["node_modules", path.resolve(__dirname, "src/_scss")]
     },
     optimization: {
@@ -33,15 +33,15 @@ module.exports = {
                 // all imported code from node_modules is a single file
                 vendor: {
                     test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
+                    name: "vendors",
+                    chunks: "all",
                     priority: 20
                 },
                 // code shared between at least 2 modules, is put into a common chunk file
                 common: {
-                    name: 'common',
+                    name: "common",
                     minChunks: 2,
-                    chunks: 'all',
+                    chunks: "all",
                     priority: 10,
                     reuseExistingChunk: true,
                     enforce: true
@@ -50,46 +50,48 @@ module.exports = {
         }
     },
     module: {
-        rules: [{
-            test: /\.js$|jsx$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader' // the babel loader tells webpack to compile JS/JSX files using Babel
-        },
-        {
-            test: /\.css$/,
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader
-                },
-                "css-loader"
-            ]
-        },
-        {
-            test: /\.scss$/,
-            use: [
-                { loader: MiniCssExtractPlugin.loader },
-                { loader: "css-loader", options: { url: true, sourceMap: !isProduction } },
-                {
-                    loader: "sass-loader",
-                    options: {
-                        url: false,
-                        sourceMap: !isProduction,
-                        includePaths: ["./src/_scss", "./node_modules"]
+        rules: [
+            {
+                test: /\.js$|jsx$/,
+                exclude: /node_modules/,
+                loader: "babel-loader" // the babel loader tells webpack to compile JS/JSX files using Babel
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: "css-loader", options: { url: true, sourceMap: !isProduction } },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            url: false,
+                            sourceMap: !isProduction,
+                            includePaths: ["./src/_scss", "./node_modules"]
+                        }
                     }
-                }
-            ]
-        },
-        {
-            test: /\.(eot|ttf|woff|woff2|png|svg|ico|gif|jpg)$/,
-            use: [
-                {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[path][name].[ext]'
+                ]
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2|png|svg|ico|gif|jpg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[path][name].[ext]"
+                        }
                     }
-                }
-            ]
-        }]
+                ]
+            }
+        ]
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -100,6 +102,10 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "[name].[contenthash].css"
         }),
-        new webpack.HashedModuleIdsPlugin() // so that file hashes don't change unexpectedly
+        new webpack.HashedModuleIdsPlugin(), // so that file hashes don't change unexpectedly
+        new webpack.NormalModuleReplacementPlugin(
+            /.*GlobalConstants/,
+            path.resolve(__dirname, "../GlobalConstants_dev.js")
+        )
     ]
 };
