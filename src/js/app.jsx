752082e407d3ddc1b6d69a2819013123490af1f6
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-06-12 17:28:00 -0400

    [dev-2847] adding 508 package

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axe from 'react-axe';
 import 'babel-polyfill';
 import AppContainer from './containers/AppContainer';
 
 const documentLocation = document.getElementById('app');
+
+if (process.env.NODE_ENV !== 'production') {
+    // logs accessbility issues to the dev console
+    axe(React, ReactDOM, 1000);
+}
 
 ReactDOM.render(
     (
