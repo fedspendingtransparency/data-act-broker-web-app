0967d90e6259f72083883dc2c08fed8edf3b36d0
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-06-14 15:48:16 -0400

    [dev-2884] adding unit test

--- tests/helpers/util-test.js
+++ tests/helpers/util-test.js
@@ -81,4 +81,21 @@ describe('util helper functions', () => {
             moment.now = () => (new Date());
         });
     });
+    describe('createOnKeyDownHandler', () => {
+        it('returns a function that is only invoked when it is passed an event with the space/enter keycodes', () => {
+            const cb = jest.fn();
+            const args = ["test1", "test2", "test3"];
+            const onKeyDownHandler = utilHelper.createOnKeyDownHandler(cb, args);
+            onKeyDownHandler({ keyCode: 1 });
+            onKeyDownHandler({ keyCode: 2 });
+            onKeyDownHandler({ keyCode: 3 });
+            onKeyDownHandler({ keyCode: 4 });
+            onKeyDownHandler({ keyCode: 5 });
+            onKeyDownHandler({ keyCode: 6 });
+            onKeyDownHandler({ keyCode: 13 }); // enter
+            onKeyDownHandler({ keyCode: 32 }); // space
+            expect(cb).toHaveBeenCalledTimes(2);
+            expect(cb).toHaveBeenCalledWith("test1", "test2", "test3");
+        });
+    });
 });
