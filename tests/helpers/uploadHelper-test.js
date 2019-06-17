e21afac1c667d5d273feb5d9094853282fd09705
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-06-17 17:27:48 -0400

    [dev-2536] tests for prepareMetaData

--- tests/helpers/uploadHelper-test.js
+++ tests/helpers/uploadHelper-test.js
@@ -1,6 +1,29 @@
-// No tests needed ATM; all API Requests
-describe('nothing to test ATM', () => {
-    it('doesn\'t require testing ATM', () => {
-        expect(true).toEqual(true);
+import * as uploadHelper from '../../src/js/helpers/uploadHelper';
+
+describe('prepareMetaData', () => {
+    const data1 = {
+        codeType: 'frec_code',
+        agency: 'test_agency',
+        startDate: 'test',
+        endDate: 'test',
+        dateType: 'quarter'
+    };
+    const data2 = {
+        codeType: 'cgac_code',
+        agency: 'test_agency2',
+        startDate: 'test',
+        endDate: 'test',
+        dateType: 'monthly'
+    };
+    it('returns the correct request object', () => {
+        const result1 = uploadHelper.prepareMetadata(data1, {});
+        expect(result1.is_quarter).toEqual(true);
+        expect(result1.frec_code).toEqual('test_agency');
+        expect(result1.cgac_code).toEqual(null);
+
+        const result2 = uploadHelper.prepareMetadata(data2, {});
+        expect(result2.is_quarter).toEqual(false);
+        expect(result2.frec_code).toEqual(null);
+        expect(result2.cgac_code).toEqual('test_agency2');
     });
 });
