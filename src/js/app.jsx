90f944410afdae3f1681a331761de299c54b8ede
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-07-01 15:49:13 -0400

    [webpack_ie_troubleshooting] removing

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axe from 'react-axe';
 import AppContainer from './containers/AppContainer';
 
 require("core-js");
@@ -8,13 +7,7 @@ require('../css/main.scss');
 
 const documentLocation = document.getElementById('app');
 
-if (process.env.NODE_ENV !== 'production') {
-    // logs accessibility issues to the dev console
-    axe(React, ReactDOM, 10000);
-}
-
 ReactDOM.render(
     <AppContainer />,
     documentLocation
 );
-
