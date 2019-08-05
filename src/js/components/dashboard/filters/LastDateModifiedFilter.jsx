87a39866e2e55216005423f931984872f0b28921
Jonathan Hill <hill_jonathan@bah.com>
2019-08-05 10:20:39 -0400

    fix linter

--- src/js/components/dashboard/filters/LastDateModifiedFilter.jsx
+++ src/js/components/dashboard/filters/LastDateModifiedFilter.jsx
@@ -11,7 +11,7 @@ const propTypes = {
     updateFilterList: PropTypes.func,
     type: PropTypes.string,
     table: PropTypes.string,
-    minDateLastModified: PropTypes.string,
+    minDateLastModified: PropTypes.string
 };
 
 const defaultProps = {
