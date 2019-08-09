a91b015076f5b68d8121fc81ac414e6b9ad07c94
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-08-09 16:51:13 -0400

    [dev-3009] using env_vars

--- GlobalConstants.js
+++ GlobalConstants.js
@@ -1,14 +1,14 @@
 export const kGlobalConstants = {
-    API: "http://localhost:9999/v1/",
+    API: process.env.API,
     LOCAL_ROOT: "http://localhost:3000",
-    CAS_ROOT: "",
-    AUTH_CALLBACK: "",
+    CAS_ROOT: process.env.CAS_ROOT,
+    AUTH_CALLBACK: process.env.API_CALLBACK,
     GITHUB:
       "https://github.com/fedspendingtransparency/data-act-validator/tree/development",
     PUBLIC_FILES: "",
-    GA_TRACKING_ID: "",
-    LOCAL: true,
-    DEV: false,
+    GA_TRACKING_ID: process.env.GA_TRACKING_ID,
+    LOCAL: false,
+    DEV: (process.env.IS_DEV === 'true'),
     STAGING: false,
-    PROD: false
+    PROD: (process.env.IS_DEV === 'false')
 };
