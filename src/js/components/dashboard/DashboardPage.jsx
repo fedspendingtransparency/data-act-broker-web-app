375e806cf4a5553a12db71e67030b178cc66c524
Lizzie Salita <salita_elizabeth@bah.com>
2019-10-07 09:29:45 -0400

    Removed banner

--- src/js/components/dashboard/DashboardPage.jsx
+++ src/js/components/dashboard/DashboardPage.jsx
@@ -6,7 +6,6 @@
 import React from 'react';
 import Navbar from 'components/SharedComponents/navigation/NavigationComponent';
 import Footer from 'components/SharedComponents/FooterComponent';
-import Banner from 'components/SharedComponents/Banner';
 import DashboardTab from './DashboardTab';
 import FilterSidebar from './FilterSidebar';
 
@@ -90,7 +89,6 @@ export default class DashboardPage extends React.Component {
                             <hr />
                         </div>
                     </div>
-                    <Banner type="dabs" />
                 </div>
                 <Footer />
             </div>
