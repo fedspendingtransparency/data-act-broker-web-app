e5fba702cb592d52f9281028250800e5f5f54f85
Lizzie Salita <salita_elizabeth@bah.com>
2019-10-15 16:50:46 -0400

    Added quarter filter components

--- src/js/components/dashboard/DashboardPage.jsx
+++ src/js/components/dashboard/DashboardPage.jsx
@@ -6,6 +6,7 @@
 import React from 'react';
 import Navbar from 'components/SharedComponents/navigation/NavigationComponent';
 import Footer from 'components/SharedComponents/FooterComponent';
+import QuarterFilterContainer from 'containers/dashboard/filters/QuarterFilterContainer';
 import DashboardTab from './DashboardTab';
 import FilterSidebar from './FilterSidebar';
 
@@ -13,7 +14,7 @@ const filters = [
     {
         name: 'Quarter',
         required: true,
-        component: null,
+        component: QuarterFilterContainer,
         description: 'Select the applicable quarter(s).'
     },
     {
---
+++ src/js/containers/dashboard/filters/QuarterFilterContainer.jsx
@@ -0,0 +1,52 @@
+/**
+ * QuarterFilterContainer.jsx
+ * Created by Lizzie Salita 10/15/19
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as filterActions from 'redux/actions/dashboard/dashboardFilterActions';
+
+import QuarterPicker from 'components/dashboard/filters/QuarterPicker';
+
+const propTypes = {
+    updateGenericFilter: PropTypes.func,
+    selectedQuarters: PropTypes.object
+};
+
+const defaultProps = {
+    selectedQuarters: []
+};
+
+class QuarterFilterContainer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.pickedQuarter = this.pickedQuarter.bind(this);
+    }
+
+    pickedQuarter(quarter) {
+        this.props.updateGenericFilter('quarters', quarter);
+    }
+
+    render() {
+        return (
+            <QuarterPicker
+                pickedQuarter={this.pickedQuarter}
+                selectedQuarters={this.props.selectedQuarters.toArray()} />
+        );
+    }
+}
+
+QuarterFilterContainer.propTypes = propTypes;
+QuarterFilterContainer.defaultProps = defaultProps;
+
+export default connect(
+    (state) => ({
+        selectedQuarters: state.dashboardFilters.quarters
+    }),
+    (dispatch) => bindActionCreators(filterActions, dispatch),
+)(QuarterFilterContainer);
