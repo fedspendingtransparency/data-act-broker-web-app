701d15f5a4b4d324214604f5fc19eb35dcaa57d0
Lizzie Salita <salita_elizabeth@bah.com>
2019-10-28 10:56:36 -0400

    added warning tooltips to quarter picker

---
+++ src/js/components/SharedComponents/WarningTooltip.jsx
@@ -0,0 +1,30 @@
+/**
+ * WarningTooltip.jsx
+ * Created by Lizzie Salita 10/26/19
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ExclamationTriangle } from 'components/SharedComponents/icons/Icons';
+
+
+const propTypes = {
+    message: PropTypes.string
+};
+
+const WarningTooltip = (props) => (
+    <div className="warning-tooltip">
+        <div className="warning-content">
+            <div className="tooltip-pointer left" />
+            <div className="icon">
+                <ExclamationTriangle alt="Warning" />
+            </div>
+            <div className="message">
+                {props.message}
+            </div>
+        </div>
+    </div>
+);
+
+WarningTooltip.propTypes = propTypes;
+export default WarningTooltip;
--- src/js/components/dashboard/filters/QuarterButton.jsx
+++ src/js/components/dashboard/filters/QuarterButton.jsx
@@ -5,6 +5,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import Tooltip from 'components/SharedComponents/WarningTooltip';
 
 const propTypes = {
     disabled: PropTypes.bool,
@@ -13,34 +14,69 @@ const propTypes = {
     pickedQuarter: PropTypes.func
 };
 
-const QuarterButton = (props) => {
-    const clickedQuarter = (e) => {
+export default class QuarterButton extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showTooltip: false
+        };
+
+        this.onMouseLeave = this.onMouseLeave.bind(this);
+        this.onMouseEnter = this.onMouseEnter.bind(this);
+    }
+
+    onMouseEnter() {
+        if (this.props.disabled) {
+            this.setState({
+                showTooltip: true
+            });
+        }
+    }
+
+    onMouseLeave() {
+        if (this.state.showTooltip) {
+            this.setState({
+                showTooltip: false
+            });
+        }
+    }
+
+    clickedQuarter(e) {
         e.preventDefault();
-        props.pickedQuarter(props.quarter);
-    };
-
-    let additionalClasses = '';
-    if (props.quarter === 1) {
-        additionalClasses += 'quarter-picker__quarter_first';
-    }
-    else if (props.quarter === 4) {
-        additionalClasses += 'quarter-picker__quarter_last';
+        this.props.pickedQuarter(this.props.quarter);
     }
 
-    if (!props.disabled && props.active) {
-        additionalClasses += ' quarter-picker__quarter_active';
-    }
+    render() {
+        let additionalClasses = '';
+        let tooltipMessage = 'The submission period has yet to open. Please search for a submission period that has closed.';
+        if (this.props.quarter === 1) {
+            additionalClasses += 'quarter-picker__quarter_first';
+            tooltipMessage = 'There is no data available for this quarter. We began recording data in Q2 of FY 17.';
+        }
+        else if (this.props.quarter === 4) {
+            additionalClasses += 'quarter-picker__quarter_last';
+        }
 
-    return (
-        <button
-            className={`quarter-picker__quarter ${additionalClasses}`}
-            disabled={props.disabled}
-            onClick={clickedQuarter}>
-            Q{props.quarter}
-        </button>
-    );
-};
+        if (!this.props.disabled && this.props.active) {
+            additionalClasses += ' quarter-picker__quarter_active';
+        }
+
+        return (
+            <button
+                className={`quarter-picker__quarter ${additionalClasses}`}
+                disabled={this.props.disabled}
+                onClick={this.clickedQuarter}
+                onMouseEnter={this.onMouseEnter}
+                onFocus={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+                onBlur={this.onMouseLeave}>
+                Q{this.props.quarter}
+                {this.state.showTooltip && <Tooltip message={tooltipMessage} />}
+            </button>
+        );
+    }
+}
 
 
 QuarterButton.propTypes = propTypes;
-export default QuarterButton;
