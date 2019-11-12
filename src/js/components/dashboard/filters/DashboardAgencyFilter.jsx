94dd02a163c8610b7dc94efd5dd87b6b6eac4ca8
alburde1 <Burdeyny_Alisa@bah.com>
2019-11-12 13:27:46 -0500

    Don't display the agency we have selected

--- src/js/components/dashboard/filters/DashboardAgencyFilter.jsx
+++ src/js/components/dashboard/filters/DashboardAgencyFilter.jsx
@@ -30,12 +30,19 @@ export default class DashboardAgencyFilter extends React.Component {
 
     render() {
         let selectedAgency = null;
-        if (Object.keys(this.props.selectedFilters.agency).length > 0) {
+        let filteredList = this.props.results;
+        if (this.props.selectedFilters.agency !== '') {
             const agencyCode = this.props.selectedFilters.agency;
-            // select only the agency we've selected
+            // select only the agency we've selected for clearing and displaying
             const agency = this.props.results.filter((result) => {
                 const code = result.cgac_code || result.frec_code;
                 return code === agencyCode;
+            });
+
+            // make the rest of the items so we don't have the chosen agency in the results list
+            filteredList = filteredList.filter((result) => {
+                const code = result.cgac_code || result.frec_code;
+                return code !== agencyCode;
             });
             // if it's a single agency user, we don't want to remove the value
             const removeVal = this.props.singleAgency ? null : this.props.onSelect.bind(null, agencyCode);
@@ -59,7 +66,7 @@ export default class DashboardAgencyFilter extends React.Component {
                         formatter={this.dataFormatter}
                         onSelect={this.props.onSelect}
                         prioritySort={false}
-                        values={this.props.results}
+                        values={filteredList}
                         clearAfterSelect
                         placeholder="Enter Agency Name" />
                 </div>
