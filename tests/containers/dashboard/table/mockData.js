dc88e229c2a4d2d62d7d79e4c7760b1cadd15501
alburde1 <Burdeyny_Alisa@bah.com>
2019-11-21 13:50:46 -0500

    Fixing some CSS and adding tests

--- tests/containers/dashboard/table/mockData.js
+++ tests/containers/dashboard/table/mockData.js
@@ -3,3 +3,45 @@ import { initialState } from 'redux/reducers/dashboard/appliedFiltersReducer';
 export const mockRedux = {
     appliedFilters: initialState
 };
+
+export const mockData = {
+    page_metadata: {
+        total: 15,
+        page: 1,
+        limit: 10
+    },
+    results: [
+        {
+            submission_id: 2,
+            fy: 2017,
+            quarter: 3,
+            rule_description: 'The description of this rule',
+            rule_label: 'TEST1',
+            instance_count: 10,
+            files: [
+                {
+                    type: "A",
+                    name: "file_A.csv"
+                }
+            ]
+        },
+        {
+            submission_id: 1,
+            fy: 2019,
+            quarter: 3,
+            rule_description: 'The description of a cross-file rule',
+            rule_label: 'TEST2',
+            instance_count: 50,
+            files: [
+                {
+                    type: "C",
+                    name: "file_C.csv"
+                },
+                {
+                    type: "D1",
+                    name: "file_D1.csv"
+                }
+            ]
+        }
+    ]
+};
