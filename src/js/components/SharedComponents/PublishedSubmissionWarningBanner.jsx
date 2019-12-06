b74d8d4490274aeaa59309118fc89eb1353f9612
Daniel Boos <boos_daniel@bah.com>
2019-12-06 10:48:44 -0500

    Missing space after period in multi-line string

--- src/js/components/SharedComponents/PublishedSubmissionWarningBanner.jsx
+++ src/js/components/SharedComponents/PublishedSubmissionWarningBanner.jsx
@@ -6,7 +6,7 @@
 import React from 'react';
 import BannerRow from './BannerRow';
 
-const warningMessage = 'This submission has already been certified.' +
+const warningMessage = 'This submission has already been certified. ' +
                        'Any changes made will not be reflected on usaspending until it is re-certified.';
 
 const PublishedSubmissionWarningBanner = () => (
