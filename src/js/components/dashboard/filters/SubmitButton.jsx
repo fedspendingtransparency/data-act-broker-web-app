aaaa1790c446dfe7c1b811ade3333f924311266f
Lizzie Salita <salita_elizabeth@bah.com>
2020-02-07 13:58:20 -0500

    requested change

--- src/js/components/dashboard/filters/SubmitButton.jsx
+++ src/js/components/dashboard/filters/SubmitButton.jsx
@@ -14,33 +14,48 @@ const propTypes = {
     type: PropTypes.oneOf(['historical', 'active'])
 };
 
-const SubmitButton = (props) => {
-    const disabled = !props.filtersChanged;
-    const title = props.filtersChanged ? 'Click to submit your search.' : 'Add or update a filter to submit.';
+export default class SubmitButton extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div
-            className="sidebar-submit"
-            role="region"
-            aria-label="Submit search">
-            <button
-                className="submit-button"
-                title={title}
-                aria-label={title}
-                disabled={disabled || !props.validFilters}
-                onClick={() => props.applyStagedFilters(props.type)}>
-                Apply Filters
-            </button>
-            <button
-                className="reset-button"
-                aria-label="Reset search"
-                onClick={() => props.resetFilters(props.type)}>
-                Reset Filters
-            </button>
-        </div>
-    );
-};
+        this.applyFilters = this.applyFilters.bind(this);
+        this.resetFilters = this.resetFilters.bind(this);
+    }
+
+    applyFilters() {
+        this.props.applyStagedFilters(this.props.type);
+    }
+
+    resetFilters() {
+        this.props.resetFilters(this.props.type);
+    }
+
+    render() {
+        const disabled = !this.props.filtersChanged;
+        const title = this.props.filtersChanged ? 'Click to submit your search.' : 'Add or update a filter to submit.';
+        return (
+            <div
+                className="sidebar-submit"
+                role="region"
+                aria-label="Submit search">
+                <button
+                    className="submit-button"
+                    title={title}
+                    aria-label={title}
+                    disabled={disabled || !this.props.validFilters}
+                    onClick={this.applyFilters}>
+                    Apply Filters
+                </button>
+                <button
+                    className="reset-button"
+                    aria-label="Reset search"
+                    onClick={this.resetFilters}>
+                    Reset Filters
+                </button>
+            </div>
+        );
+    }
+}
 
 SubmitButton.propTypes = propTypes;
 
-export default SubmitButton;
