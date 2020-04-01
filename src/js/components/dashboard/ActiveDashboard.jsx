8dbce0bb246d1363cbd388b68734bc231c14a5bd
alburde1 <Burdeyny_Alisa@bah.com>
2020-04-01 13:37:33 -0400

    Adding an active dashboard table

--- src/js/components/dashboard/ActiveDashboard.jsx
+++ src/js/components/dashboard/ActiveDashboard.jsx
@@ -7,6 +7,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ActiveDashboardOverviewContainer from 'containers/dashboard/ActiveDashboardOverviewContainer';
+import ActiveDashboardTableContainer from 'containers/dashboard/table/ActiveDashboardTableContainer';
 
 const propTypes = {
     submissionID: PropTypes.string
@@ -19,6 +20,7 @@ const ActiveDashboard = (props) => (
             <h2>Active Submission Summary</h2>
             <hr />
             {props.submissionID}
+            <ActiveDashboardTableContainer submissionID={props.submissionID} />
         </div>
     </div>
 );
