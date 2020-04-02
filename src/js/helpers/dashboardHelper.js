3783b434d810e54cd94e0f0cd4794a428e4894de
Daniel Boos <boos_daniel@bah.com>
2020-04-02 14:49:34 -0400

    Adding get_impacts_counts helper

--- src/js/helpers/dashboardHelper.js
+++ src/js/helpers/dashboardHelper.js
@@ -122,3 +122,21 @@ export const fetchActiveOverview = (submissionId, fileType, errorLevel) => {
 
     return deferred.promise;
 };
+
+export const fetchActiveImpacts = (submissionId, fileType, errorLevel) => {
+    const deferred = Q.defer();
+
+    Request.get(`${kGlobalConstants.API}get_impact_counts/?submission_id=${submissionId}&file=${fileType}&error_level=${errorLevel}`)
+        .end((err, res) => {
+            if (err) {
+                const response = Object.assign({}, res.body);
+                response.httpStatus = res.status;
+                deferred.reject(response);
+            }
+            else {
+                deferred.resolve(res.body);
+            }
+        });
+
+    return deferred.promise;
+};
