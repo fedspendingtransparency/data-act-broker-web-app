c169c9eac89625ebee21eb06fdbc57f24d923fc4
alburde1 <Burdeyny_Alisa@bah.com>
2020-04-10 14:46:33 -0400

    Adding modal description and tweaking some CSS

--- src/js/components/settings/SettingsModal.jsx
+++ src/js/components/settings/SettingsModal.jsx
@@ -58,6 +58,10 @@ export default class SettingsModal extends React.Component {
                             </div>
                             <div className="settings-modal__main">
                                 <h2>Rule Settings</h2>
+                                <div className="description">
+                                    Change the Significance and Impact settings to alter the weight of your agency's
+                                    respective rules in the Active Dashboard chart.
+                                </div>
                             </div>
                         </div>
                     </div>
