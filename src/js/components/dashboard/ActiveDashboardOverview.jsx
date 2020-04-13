8d7404d38e0cf822300abce8ce8ad48e2aec02c5
Daniel Boos <boos_daniel@bah.com>
2020-04-13 19:01:57 -0400

    Updating ActiveDashboardOverview for Error, None, Loading states

--- src/js/components/dashboard/ActiveDashboardOverview.jsx
+++ src/js/components/dashboard/ActiveDashboardOverview.jsx
@@ -5,66 +5,84 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import ErrorMessageOverlay from 'components/SharedComponents/ErrorMessageOverlay';
+import LoadingMessage from 'components/SharedComponents/LoadingMessage';
 
 const propTypes = {
     submissionData: PropTypes.object,
-    errorLevel: PropTypes.string
+    errorLevel: PropTypes.string,
+    inFlight: PropTypes.bool,
+    hasFailed: PropTypes.bool
 };
 
-const ActiveDashboardOverview = (props) => (
-    <div className="dashboard-page__submission-overview">
-        {/* eslint-disable import/no-dynamic-require, global-require */}
-        <h2>
-            {props.submissionData.icon_name ?
-                <img
-                    src={require(`../../../graphics/agency/${props.submissionData.icon_name}`)}
-                    alt={props.submissionData.agency_name} /> : ''}
-            {props.submissionData.agency_name}
-        </h2>
-        {/* eslint-enable import/no-dynamic-require, global-require */}
-        <hr />
-        <div className="submission-overview-details">
-            <h3>Submission Period</h3>
-            <div className="flex-wrapper">
-                <div className="overview-section">
-                    <h4>Certification Due</h4>
-                    {props.submissionData.certification_deadline}
-                </div>
-                <div className="overview-section">
-                    <h4>Days Left Until Certification</h4>
-                    {props.submissionData.days_remaining}
-                </div>
-                <div className="overview-section">
-                    <h4>Reporting Period</h4>
-                    {props.submissionData.reporting_period}
-                </div>
-                <div className="overview-section">
-                    <h4>Duration</h4>
-                    {props.submissionData.duration}
+export default class ActiveDashboardOverview extends React.Component {
+    render() {
+        let content = (
+            <div>
+                {/* eslint-disable import/no-dynamic-require, global-require */}
+                <h2>
+                    {this.props.submissionData.icon_name ?
+                        <img
+                            src={require(`../../../graphics/agency/${this.props.submissionData.icon_name}`)}
+                            alt={this.props.submissionData.agency_name} /> : ''}
+                    {this.props.submissionData.agency_name || 'Submission Data' }
+                </h2>
+                {/* eslint-enable import/no-dynamic-require, global-require */}
+                <hr />
+                <div className="submission-overview-details">
+                    <h3>Submission Period</h3>
+                    <div className="flex-wrapper">
+                        <div className="overview-section">
+                            <h4>Certification Due</h4>
+                            {this.props.submissionData.certification_deadline || 'N/A' }
+                        </div>
+                        <div className="overview-section">
+                            <h4>Days Left Until Certification</h4>
+                            {this.props.submissionData.days_remaining || 'N/A' }
+                        </div>
+                        <div className="overview-section">
+                            <h4>Reporting Period</h4>
+                            {this.props.submissionData.reporting_period || 'N/A' }
+                        </div>
+                        <div className="overview-section">
+                            <h4>Duration</h4>
+                            {this.props.submissionData.duration || 'N/A' }
+                        </div>
+                    </div>
+                    <h3>Submission Status ({this.props.errorLevel}s)</h3>
+                    <div className="flex-wrapper">
+                        <div className="overview-section">
+                            <h4>Submission ID</h4>
+                            {this.props.submissionData.submission_id || 'N/A' }
+                        </div>
+                        <div className="overview-section">
+                            <h4>Validation Type</h4>
+                            {this.props.submissionData.file || 'N/A' }
+                        </div>
+                        <div className="overview-section">
+                            <h4>Number of Rules</h4>
+                            {this.props.submissionData.number_of_rules || 'N/A' }
+                        </div>
+                        <div className="overview-section">
+                            <h4>Total # of Warings</h4>
+                            {this.props.submissionData.total_instances || 'N/A' }
+                        </div>
+                    </div>
                 </div>
             </div>
-            <h3>Submission Status ({props.errorLevel}s)</h3>
-            <div className="flex-wrapper">
-                <div className="overview-section">
-                    <h4>Submission ID</h4>
-                    {props.submissionData.submission_id}
-                </div>
-                <div className="overview-section">
-                    <h4>Validation Type</h4>
-                    {props.submissionData.file}
-                </div>
-                <div className="overview-section">
-                    <h4>Number of Rules</h4>
-                    {props.submissionData.number_of_rules}
-                </div>
-                <div className="overview-section">
-                    <h4>Total # of Warings</h4>
-                    {props.submissionData.total_instances}
-                </div>
+        );
+        if (this.props.inFlight) {
+            content = <LoadingMessage />;
+        }
+        else if (this.props.hasFailed) {
+            content = <ErrorMessageOverlay />;
+        }
+        return (
+            <div className="dashboard-page__submission-overview">
+                { content }
             </div>
-        </div>
-    </div>
-);
+        );
+    }
+}
 
 ActiveDashboardOverview.propTypes = propTypes;
-export default ActiveDashboardOverview;
