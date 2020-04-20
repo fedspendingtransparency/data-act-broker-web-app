68e896b1771eff8f4f416a1ed91cd24aab5bb229
Daniel Boos <boos_daniel@bah.com>
2020-04-20 18:11:33 -0400

    Updating BannerRow, SubmissionWarningBanner to handle JSX, for revertSubmission

--- src/js/components/SharedComponents/SubmissionWarningBanner.jsx
+++ src/js/components/SharedComponents/SubmissionWarningBanner.jsx
@@ -5,61 +5,73 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import { Link } from 'react-router-dom';
 import BannerRow from './BannerRow';
 
 const propTypes = {
-    submission: PropTypes.object
+    submissionInfo: PropTypes.object,
+    revertSubmission: PropTypes.func
 };
 
 const defaultProps = {
-    submission: null
+    submissionInfo: null,
+    revertSubmission: null
 };
 
 export default class SubmissionWarningBanner extends React.Component {
     render() {
         let content = '';
-        let link = null;
-        let warningBannerType = this.props.submission.publishStatus;
-        if (this.props.submission.info.certifiedSubmission) {
-            warningBannerType = 'test-submission';
-            link = `https://broker.usaspending.gov/#/submission/${this.props.submission.info.certified_submission}/`;
-        }
-        if (warningBannerType === 'updated') {
-            link = `https://broker.usaspending.gov/#/submission/${this.props.submission.id}/`;
-        }
-
-        const warningBannerMap = {
-            unpublished: null,
-            'test-submission': {
-                header: 'Submission Already Certified for This Fiscal Quarter',
-                message: `You cannot certify this submission since one has already been certified for this fiscal quarter.` +
-                         ` To view the certified submission, click [here](${link}).`
-            },
-            published: {
-                header: 'Submission Already Certified',
-                message: 'This submission has already been certified.' +
-                         ' Any changes made will not be reflected on usaspending until it is re-certified.'
-            },
-            publishing: {
-                header: 'Submission Currently Certifying',
-                message: 'This submission is currently certifying.' +
-                         ' No changes can be made until the certification is complete.'
-            },
-            updated: {
-                header: 'Submission Updated',
-                message: `This submission has been updated.` +
-                         ` If you would like changes to be reflected on USASpending, please certify.` +
-                         ` If you would to undo these changes, click [here](${link}).`
-            },
-            reverting: {
-                header: 'Submission Currently Reverting',
-                message: 'This submission is currently reverting.' +
-                         ' No changes can be made until reverting is complete.'
+        if (this.props.submissionInfo != null) {
+            let warningBannerType = this.props.submissionInfo.publish_status;
+            if (this.props.submissionInfo.certified_submission) {
+                warningBannerType = 'test-submission';
             }
-        };
-        const warningBanner = warningBannerMap[warningBannerType];
-        if (warningBanner) {
-            content = <BannerRow type="warning" {...warningBanner} />;
+
+            const warningBannerMap = {
+                unpublished: null,
+                'test-submission': {
+                    header: (<p>Submission Already Certified for This Fiscal Quarter</p>),
+                    message: (
+                        <p>
+                            You cannot certify this submission since one has already been certified for this fiscal quarter.
+                            To view the certified submission, click <Link to={`/submission/${this.props.submissionInfo.certified_submission}/`}>here</Link>.
+                        </p>),
+                    useMarkdown: false
+                },
+                published: {
+                    header: 'Submission Already Certified',
+                    message: 'This submission has already been certified.' +
+                            ' Any changes made will not be reflected on usaspending until it is re-certified.',
+                    useMarkdown: true
+                },
+                publishing: {
+                    header: 'Submission Currently Certifying',
+                    message: 'This submission is currently certifying.' +
+                            ' No changes can be made until the certification is complete.',
+                    useMarkdown: true
+                },
+                updated: {
+                    header: (<p>Submission Updated</p>),
+                    message: (
+                        <p>
+                            This submission has been updated.
+                            If you would like changes to be reflected on USASpending, please certify.
+                            If you would to undo these changes, click <button className="link-button" onClick={this.props.revertSubmission}>here</button>.
+                        </p>),
+                    useMarkdown: false
+                },
+                reverting: {
+                    header: 'Submission Currently Reverting',
+                    message: 'This submission is currently reverting.' +
+                            ' No changes can be made until reverting is complete.',
+                    useMarkdown: true
+                }
+            };
+            const warningBanner = warningBannerMap[warningBannerType];
+            if (warningBanner) {
+                content = <BannerRow type="warning" {...warningBanner} />;
+            }
         }
         return content;
     }
