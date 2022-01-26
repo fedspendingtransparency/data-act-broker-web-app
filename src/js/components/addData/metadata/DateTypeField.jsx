d463c836a7e5a0591b9da7c5d4048074caf5a732
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2022-01-26 11:53:08 -0500

    Adding note about quarterly submissions only being available <=FY21

--- src/js/components/addData/metadata/DateTypeField.jsx
+++ src/js/components/addData/metadata/DateTypeField.jsx
@@ -5,6 +5,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const propTypes = {
     onChange: PropTypes.func,
@@ -73,6 +74,9 @@ export default class DateTypeField extends React.Component {
                                 Quarterly
                             </label>
                         </div>
+                    </div>
+                    <div className="quarterly-submission-note">
+                        <b>Note:</b> Quarterly submissions are only available for FY21 and prior.
                     </div>
                 </div>
             </div>
