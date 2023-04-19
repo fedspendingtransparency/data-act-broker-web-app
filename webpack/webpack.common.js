0ac55d68abd2e0522e98796034c364afc372c3e8
Nick Torres <nicholas.torres@kc.frb.org>
2023-04-19 13:20:29 -0600

    this might work

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,48 +4,28 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const GitRevisionPlugin = require("git-revision-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const gitRevisionPlugin = new GitRevisionPlugin({ branch: true }); // 'rev-parse HEAD' is default command to find latest commit
 
-console.log("Commit Hash for this build: ", gitRevisionPlugin.commithash());
-console.log("Branch for this build: ", gitRevisionPlugin.branch());
-
 module.exports = {
-    entry: path.resolve(__dirname, "../src/js/app"),
-    context: path.resolve(__dirname, "../src"),
+    entry: {
+        app: "../src/index.js"
+    },
     output: {
         // https://webpack.js.org/guides/caching/
         publicPath: "/",
         filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "../public")
     },
+    context: path.resolve(__dirname, "../src"),
     resolve: {
         extensions: [".js", ".jsx", ".md", ".mdx"],
-        modules: ["node_modules", path.resolve(__dirname, "src/_scss")]
+        modules: ["node_modules", path.resolve(__dirname, "src/_scss")],
+        fallback: { querystring: require.resolve("querystring-es3") }
     },
     optimization: {
-        splitChunks: {
-            cacheGroups: {
-                default: false,
-                vendors: false,
-                // all imported code from node_modules is a single file
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendors",
-                    chunks: "all",
-                    priority: 20
-                },
-                // code shared between at least 2 modules, is put into a common chunk file
-                common: {
-                    name: "common",
-                    minChunks: 2,
-                    chunks: "all",
-                    priority: 10,
-                    reuseExistingChunk: true,
-                    enforce: true
-                }
-            }
-        },
+        splitChunks: { chunks: 'all' },
         moduleIds: 'deterministic' // so that file hashes don't change unexpectedly
     },
     module: {
@@ -80,7 +60,8 @@ module.exports = {
         new CleanWebpackPlugin(),
         new webpack.IgnorePlugin({
             resourceRegExp: /^\.\/locale$/,
-            contextRegExp: /moment$/}),
+            contextRegExp: /moment$/
+        }),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "../src/index.html"),
             favicon: path.resolve(__dirname, "../src/graphics/favicon.ico")
@@ -88,6 +69,35 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "[name].[contenthash].css"
         }),
+        // new webpack.HashedModuleIdsPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: '*.xml',
+                    to: path.resolve(__dirname, "../public"),
+                    context: path.resolve(__dirname, '../'),
+                    noErrorOnMissing: true
+                },
+                {
+                    from: '*.xml',
+                    to: path.resolve(__dirname, "../public"),
+                    context: path.resolve(__dirname, '../'),
+                    noErrorOnMissing: true
+                },
+                {
+                    from: 'robots.txt',
+                    to: path.resolve(__dirname, "../public"),
+                    context: path.resolve(__dirname, '../'),
+                    noErrorOnMissing: true
+                },
+                {
+                    from: 'redirect-config.json',
+                    to: path.resolve(__dirname, "../public"),
+                    context: path.resolve(__dirname, '../'),
+                    noErrorOnMissing: true
+                }
+            ]
+        })
         // new webpack.HashedModuleIdsPlugin() // so that file hashes don't change unexpectedly
     ]
 };
