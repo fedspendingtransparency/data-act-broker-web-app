614d2f230351d89d4b7a405ca7e0f50d9c23d441
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2023-06-15 10:28:36 -0400

    Updating scope and redirect_uri

--- src/js/components/login/LoginMax.jsx
+++ src/js/components/login/LoginMax.jsx
@@ -71,10 +71,11 @@ export default class LoginMax extends React.Component {
     handleCaiaClick(e) {
         if (e.keyCode === '13' || !e.keyCode) {
             // TODO: update scope appropriately
-            const scope = "email";
+            const scope = "openid email profile address phone";
             const url = `${kGlobalConstants.CAIA_ROOT}/as/authorization.oauth2?`
                 + `response_type=code`
                 + `&scope=${scope}`
+                + `&redirect_uri=${encodeURIComponent(kGlobalConstants.AUTH_CALLBACK)}`
                 + `&client_id=${encodeURIComponent(kGlobalConstants.CAIA_CLIENT)}`;
             console.log(url);
             window.location.assign(url);
