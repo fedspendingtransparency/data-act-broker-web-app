7c24e981622536aca42c6c5c3e63357ebff515ea
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2023-06-15 11:09:28 -0400

    Updating auth page to use the correct login endpoint

--- src/js/helpers/loginHelper.js
+++ src/js/helpers/loginHelper.js
@@ -171,6 +171,58 @@ export const performMaxLogin = (ticket) => {
     return deferred.promise;
 };
 
+export const performCAIALogin = (code) => {
+    const deferred = Q.defer();
+    const store = new StoreSingleton().store;
+
+    // wipe out old session cookies to prevent session weirdness
+    Cookies.remove('session');
+    // determine the service
+    const service = encodeURIComponent(kGlobalConstants.AUTH_CALLBACK);
+
+    Request.post(`${kGlobalConstants.API}caia_login/`)
+        .send({ code, service })
+        .end((err, res) => {
+            if (err) {
+                const action = sessionActions.setLoginState('failed');
+                store.dispatch(action);
+
+                // unset the login state cookie
+                Cookies.remove('brokerLogin');
+
+                // if a message is available, display that
+                if (res && Object.prototype.hasOwnProperty.call(res, 'body')
+                && Object.prototype.hasOwnProperty.call(res.body, 'message')) {
+                    deferred.reject(res.body.message);
+                }
+                else {
+                    deferred.reject(err);
+                }
+            }
+            else {
+                Cookies.set('brokerLogin', Date.now(), { expires: (1 / (24 * 4)) });
+                establishSession(res.headers);
+                // check if cookies could be set
+                if (!Cookies.get('session')) {
+                    // couldn't set cookie, fail this request and notify the user
+                    const action = sessionActions.setLoginState('failed');
+                    store.dispatch(action);
+                    deferred.reject('cookie');
+                    return;
+                }
+
+                fetchActiveUser(store)
+                    .then((data) => {
+                        deferred.resolve(data);
+                    })
+                    .catch((dataErr) => {
+                        deferred.reject(dataErr);
+                    });
+            }
+        });
+    return deferred.promise;
+};
+
 export const performLogout = () => {
     const deferred = Q.defer();
 
