96801456d6043404880c8d69dc62c8b492725a86
alburde1 <alisa.burdeyny@kc.frb.org>
2025-06-27 16:08:02 -0400

    Fixing infinite refresh loop

--- src/js/containers/router/RouterContainer.jsx
+++ src/js/containers/router/RouterContainer.jsx
@@ -4,32 +4,10 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-import * as sessionActions from '../../redux/actions/sessionActions';
 import RouterRoutes from './RouterRoutes';
 
 import WithAuth from "./WithAuth";
-
-const propTypes = {
-    session: PropTypes.shape({
-        login: PropTypes.string,
-        user: PropTypes.shape({
-            affiliations: PropTypes.arrayOf(
-                PropTypes.shape({
-                    permission: PropTypes.string
-                })
-            )
-        })
-    })
-};
-
-const defaultProps = {
-    session: {}
-};
 
 const RouteList = new RouterRoutes();
 
@@ -47,8 +25,7 @@ class RouterContainer extends React.Component {
                             <Route
                                 key={route.path}
                                 path={route.path}
-                                element={<Component />}
-                                session={this.props.session} />
+                                element={<Component />} />
                         )})
                     ]}
                 </Routes>
@@ -57,12 +34,4 @@ class RouterContainer extends React.Component {
     }
 }
 
-RouterContainer.propTypes = propTypes;
-RouterContainer.defaultProps = defaultProps;
-
-export default connect(
-    (state) => ({
-        session: state.session
-    }),
-    (dispatch) => bindActionCreators(sessionActions, dispatch)
-)(RouterContainer);
+export default RouterContainer;
