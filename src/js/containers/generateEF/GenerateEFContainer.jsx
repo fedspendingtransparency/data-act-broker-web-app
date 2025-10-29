ee8251caeed0a2d704669bb7e24abee85052d418
alburde1 <alisa.burdeyny@kc.frb.org>
2025-10-29 14:56:57 -0400

    Updating generateEFContainer to functional component

--- src/js/containers/generateEF/GenerateEFContainer.jsx
+++ src/js/containers/generateEF/GenerateEFContainer.jsx
@@ -3,10 +3,10 @@
 * Created by Kevin Li 8/23/16
 */
 
-import React from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { useState, useEffect } from 'react';
 
 import * as uploadActions from 'redux/actions/uploadActions';
 import * as GenerateHelper from 'helpers/generateFilesHelper';
@@ -21,65 +21,56 @@ const propTypes = {
     errorFromStep: PropTypes.func
 };
 
-const defaultProps = {
-    submission: {},
-    submissionID: ""
-};
-
 const timerDuration = 10;
 
-class GenerateEFContainer extends React.Component {
-    constructor(props) {
-        super(props);
+const GenerateEFContainer = ({submission = {}, submissionID = '', ...props}) => {
+    const [fullPageError, setFullPageError] = useState(false);
+    const [fullPageMessage, setFullPageMessage] = useState('');
+    const [isReady, setIsReady] = useState(false);
+    const [hasErrors, setHasErrors] = useState(false);
+    const [fileE, setFileE] = useState({});
+    const [fileF, setFileF] = useState({});
+    const [generated, setGenerated] = useState(false);
+    const [agency_name, setAgencyName] = useState('');
+    const [isUnmounted, setIsUnmounted] = useState(false);
 
-        this.isUnmounted = false;
+    useEffect(() => {
+        setIsUnmounted(false);
+        updateAgencyName();
+        checkFileStatus();
 
-        this.state = {
-            fullPageError: false,
-            fullPageMessage: '',
-            isReady: false,
-            hasErrors: false,
-            e: {},
-            f: {},
-            generated: false,
-            agency_name: ''
+        return () => {
+            setIsUnmounted(true);
         };
-    }
+    }, []);
 
-    componentDidMount() {
-        this.isUnmounted = false;
-        this.setAgencyName(this.props);
-        this.checkFileStatus();
-    }
+    useEffect(() => {
+        updateAgencyName();
+    }, [submissionID]);
 
-    componentDidUpdate(prevProps) {
-        if (this.props.submissionID !== prevProps.submissionID) {
-            this.setAgencyName(this.props);
-        }
-    }
+    // We only parse state after files E and F have been updated, just checking for file F for simplicity
+    useEffect(() => {
+        parseState()
+    }, [fileF]);
 
-    componentWillUnmount() {
-        this.isUnmounted = true;
-    }
-
-    setAgencyName(givenProps) {
-        if (givenProps.submissionID !== null) {
-            ReviewHelper.fetchSubmissionMetadata(givenProps.submissionID)
+    const updateAgencyName = () => {
+        if (submissionID !== null) {
+            ReviewHelper.fetchSubmissionMetadata(submissionID)
                 .then((res) => {
-                    if (!this.isUnmounted) {
-                        this.props.setSubmissionPublishStatus(res.data.publish_status);
-                        this.setState({ agency_name: res.data.agency_name });
+                    if (!isUnmounted) {
+                        props.setSubmissionPublishStatus(res.data.publish_status);
+                        setAgencyName(res.data.agency_name);
                     }
                 })
                 .catch((error) => {
                     console.error(error);
-                    this.props.errorFromStep(error.response.data.message);
+                    props.errorFromStep(error.response.data.message);
                 });
         }
     }
 
-    handleResponse(allResponses) {
-        if (this.isUnmounted) {
+    const handleResponse = (allResponses) => {
+        if (isUnmounted) {
             return;
         }
 
@@ -92,9 +83,9 @@ class GenerateEFContainer extends React.Component {
                 data = response.value.data;
 
                 if (data.status === 'invalid') {
-                    if (!this.state.generated) {
-                        this.setState({ generated: true });
-                        this.generateFiles();
+                    if (!generated) {
+                        setGenerated(true);
+                        generateFiles();
                         return;
                     }
                     data.message = 'Prerequisites required to generate this file are incomplete.';
@@ -106,89 +97,85 @@ class GenerateEFContainer extends React.Component {
 
             newState[keys[i]] = data;
         }
-
-        this.setState(newState, this.parseState);
+        setFileE(newState['e']);
+        setFileF(newState['f']);
     }
 
-    generateFiles() {
+    const generateFiles = () => {
         Promise.allSettled([
-            GenerateHelper.generateFile('E', this.props.submissionID, '', ''),
-            GenerateHelper.generateFile('F', this.props.submissionID, '', '')
+            GenerateHelper.generateFile('E', submissionID, '', ''),
+            GenerateHelper.generateFile('F', submissionID, '', '')
         ]).then((allResponses) => {
-            this.handleResponse(allResponses);
+            handleResponse(allResponses);
         });
     }
 
-    checkFileStatus() {
+    const checkFileStatus = () => {
         Promise.allSettled([
-            GenerateHelper.checkGenerationStatus('E', this.props.submissionID),
-            GenerateHelper.checkGenerationStatus('F', this.props.submissionID)
+            GenerateHelper.checkGenerationStatus('E', submissionID),
+            GenerateHelper.checkGenerationStatus('F', submissionID)
         ]).then((allResponses) => {
-            this.handleResponse(allResponses);
+            handleResponse(allResponses);
         });
     }
 
-    parseState() {
-        const files = [this.state.e, this.state.f];
-        let hasErrors = false;
-        let isReady = true;
+    const parseState = () => {
+        const files = [fileE, fileF];
+        let tmpHasErrors = false;
+        let tmpIsReady = true;
         let showFullPageError = false;
-        let fullPageMessage = '';
+        let tmpFullPageMessage = '';
         files.forEach((file) => {
             if (file.httpStatus === 403 || file.httpStatus === 401) {
                 // permissions error, reject
                 showFullPageError = true;
-                fullPageMessage = 'You are not authorized to perform the requested task. ' +
+                tmpFullPageMessage = 'You are not authorized to perform the requested task. ' +
                     'Please contact your administrator.';
             }
-            else if (file.status === 'failed') {
-                hasErrors = true;
+            else if (file.status === 'failed' || file.status === 'invalid') {
+                tmpHasErrors = true;
             }
             else if (file.status === 'waiting') {
-                isReady = false;
-            }
-            else if (file.status === 'invalid') {
-                isReady = true;
-                hasErrors = true;
+                tmpIsReady = false;
             }
         });
 
-        this.setState({
-            fullPageError: showFullPageError,
-            fullPageMessage,
-            hasErrors,
-            isReady
-        });
+        setFullPageError(showFullPageError);
+        setFullPageMessage(tmpFullPageMessage);
+        setHasErrors(tmpHasErrors);
+        setIsReady(tmpIsReady);
 
-        if (!isReady && !this.isUnmounted) {
+        if (!tmpIsReady && !isUnmounted) {
             // files aren't ready yet, keep checking
             window.setTimeout(() => {
-                this.checkFileStatus();
+                checkFileStatus();
             }, timerDuration * 1000);
         }
     }
 
-    render() {
-        let content = (<GenerateEFContent
-            {...this.props}
-            {...this.state}
-            publishStatus={this.props.submission.publishStatus}
-            generateFiles={this.generateFiles.bind(this)} />);
+    let content = (<GenerateEFContent
+        submissionID={submissionID}
+        {...props}
+        isReady={isReady}
+        hasErrors={hasErrors}
+        e={fileE}
+        f={fileF}
+        agency_name={agency_name}
+        publishStatus={submission.publishStatus}
+        generateFiles={generateFiles} />);
 
-        if (this.state.fullPageError) {
-            content = <GenerateEFError message={this.state.fullPageMessage} />;
-        }
-
-        return (
-            <div className="generate-ef-page">
-                {content}
-            </div>
-        );
+    if (fullPageError) {
+        content = <GenerateEFError message={fullPageMessage} />;
     }
-}
+
+    return (
+        <div className="generate-ef-page">
+            {content}
+        </div>
+    );
+};
 
 GenerateEFContainer.propTypes = propTypes;
-GenerateEFContainer.defaultProps = defaultProps;
 
 export default connect(
     (state) => ({
