6c185f658ce55981cca5ecc4354f9ac3adc0393f
alburde1 <alisa.burdeyny@kc.frb.org>
2025-12-04 12:17:23 -0500

    Update very basic validateData components to be functional components

--- src/js/components/validateData/validateValues/ValidateValuesTreemapHelpPlaceholder.jsx
+++ src/js/components/validateData/validateValues/ValidateValuesTreemapHelpPlaceholder.jsx
@@ -3,26 +3,19 @@
   * Created by Kevin Li 6/20/16
   */
 
-import React from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
     type: PropTypes.string
 };
 
-const defaultProps = {
-    type: 'error'
+const ValidateValuesTreemapHelpPlaceholder = ({type = 'error'}) => {
+    return (
+        <div className="usa-da-treemap-help-wrap">
+            Click on a block for more information about the {type}.
+        </div>
+    );
 };
 
-export default class ValidateValuesTreemapHelpPlaceholder extends React.Component {
-    render() {
-        return (
-            <div className="usa-da-treemap-help-wrap">
-                Click on a block for more information about the {this.props.type}.
-            </div>
-        );
-    }
-}
-
 ValidateValuesTreemapHelpPlaceholder.propTypes = propTypes;
-ValidateValuesTreemapHelpPlaceholder.defaultProps = defaultProps;
+export default ValidateValuesTreemapHelpPlaceholder;
