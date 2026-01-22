fe8f2bc5260d9f25deebbf399f6e25385ef2a7c0
alburde1 <alisa.burdeyny@kc.frb.org>
2026-01-22 12:48:35 -0500

    Indentation lint errors

--- src/js/components/submission/SubmissionPage.jsx
+++ src/js/components/submission/SubmissionPage.jsx
@@ -30,68 +30,68 @@ const propTypes = {
 const SubmissionPage = ({
     error = false, errorMessage = '', loading = true, submissionInfo = {}, currentStep = 0, reverting = false, ...props
 }) => {
-        let content;
-        switch (currentStep) {
-            case 1:
-                content = (
-                    <ValidateDataContainer
-                        submissionID={props.submissionID}
-                        errorFromStep={props.errorFromStep} />
-                );
-                break;
-            case 2:
-                content = (
-                    <GenerateFilesContainer
-                        submissionID={props.submissionID}
-                        errorFromStep={props.errorFromStep} />
-                );
-                break;
-            case 3:
-                content = (
-                    <CrossFileContentContainer
-                        submissionID={props.submissionID}
-                        errorFromStep={props.errorFromStep}
-                        publishStatus={submissionInfo.publish_status} />
-                );
-                break;
-            case 4:
-                content = <GenerateEFContainer submissionID={props.submissionID} errorFromStep={props.errorFromStep} />;
-                break;
-            case 5:
-                content = (<ReviewDataContainer
+    let content;
+    switch (currentStep) {
+        case 1:
+            content = (
+                <ValidateDataContainer
+                    submissionID={props.submissionID}
+                    errorFromStep={props.errorFromStep} />
+            );
+            break;
+        case 2:
+            content = (
+                <GenerateFilesContainer
+                    submissionID={props.submissionID}
+                    errorFromStep={props.errorFromStep} />
+            );
+            break;
+        case 3:
+            content = (
+                <CrossFileContentContainer
                     submissionID={props.submissionID}
                     errorFromStep={props.errorFromStep}
-                    testSubmission={submissionInfo.test_submission} />);
-                break;
-            default:
-                content = null;
-        }
-        const subStatusBanner = (<SubmissionWarningBanner
-            submissionInfo={submissionInfo}
-            revertSubmission={props.revertSubmission}
-            reverting={reverting} />);
-        return (
-            <div className="usa-da-submission-page">
-                <Navbar activeTab="submissionGuide" type="dabs" />
-                <main>
-                    <SubmissionHeader {...submissionInfo} />
-                    <Banner type="dabs" />
-                    <div className="usa-da-content-step-block" name="content-top">
-                        <div className="container center-block">
-                            <div className="row">
-                                <Progress
-                                    currentStep={currentStep}
-                                    id={props.submissionID} />
-                            </div>
+                    publishStatus={submissionInfo.publish_status} />
+            );
+            break;
+        case 4:
+            content = <GenerateEFContainer submissionID={props.submissionID} errorFromStep={props.errorFromStep} />;
+            break;
+        case 5:
+            content = (<ReviewDataContainer
+                submissionID={props.submissionID}
+                errorFromStep={props.errorFromStep}
+                testSubmission={submissionInfo.test_submission} />);
+            break;
+        default:
+            content = null;
+    }
+    const subStatusBanner = (<SubmissionWarningBanner
+        submissionInfo={submissionInfo}
+        revertSubmission={props.revertSubmission}
+        reverting={reverting} />);
+    return (
+        <div className="usa-da-submission-page">
+            <Navbar activeTab="submissionGuide" type="dabs" />
+            <main>
+                <SubmissionHeader {...submissionInfo} />
+                <Banner type="dabs" />
+                <div className="usa-da-content-step-block" name="content-top">
+                    <div className="container center-block">
+                        <div className="row">
+                            <Progress
+                                currentStep={currentStep}
+                                id={props.submissionID} />
                         </div>
                     </div>
-                    {subStatusBanner}
-                    {error ? (<DABSFABSErrorMessage message={errorMessage} />) : null}
-                    {loading ? (<ReviewLoading />) : null}
-                    {content}
-                </main>
-            </div>
-        );
+                </div>
+                {subStatusBanner}
+                {error ? (<DABSFABSErrorMessage message={errorMessage} />) : null}
+                {loading ? (<ReviewLoading />) : null}
+                {content}
+            </main>
+        </div>
+    );
 };
 
 SubmissionPage.propTypes = propTypes;
