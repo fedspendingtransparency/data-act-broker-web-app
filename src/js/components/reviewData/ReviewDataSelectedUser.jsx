a01020b5466793572c7f1566af1c2af07d68484e
alburde1 <alisa.burdeyny@kc.frb.org>
2026-04-10 10:54:39 -0400

    Updating the most simple reviewData components to functional ones

--- src/js/components/reviewData/ReviewDataSelectedUser.jsx
+++ src/js/components/reviewData/ReviewDataSelectedUser.jsx
@@ -3,7 +3,6 @@
  * Created by Mike Bray 6/5/16
  */
 
-import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -12,25 +11,18 @@ const propTypes = {
     user: PropTypes.object
 };
 
-const defaultProps = {
-    deselectUser: null,
-    user: null
+const ReviewDataSelectedUser = ({deselectUser = null, user = null}) => {
+    return (
+        <div className="usa-da-review-data-user-row">
+            <button className="usa-da-icon usa-da-icon-times-circle" onClick={deselectUser} alt="Remove">
+                <FontAwesomeIcon icon="circle-xmark" />
+            </button>
+            <span>
+                {user.displayName}
+            </span>
+        </div>
+    );
 };
 
-export default class ReviewDataSelectedUser extends React.Component {
-    render() {
-        return (
-            <div className="usa-da-review-data-user-row">
-                <button className="usa-da-icon usa-da-icon-times-circle" onClick={this.props.deselectUser} alt="Remove">
-                    <FontAwesomeIcon icon="circle-xmark" />
-                </button>
-                <span>
-                    {this.props.user.displayName}
-                </span>
-            </div>
-        );
-    }
-}
-
 ReviewDataSelectedUser.propTypes = propTypes;
-ReviewDataSelectedUser.defaultProps = defaultProps;
+export default ReviewDataSelectedUser;
